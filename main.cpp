cd2b35d6d367aefb3ea9ebf54cba534c3aa50714
EarthOL-088217 <90408077+EarthOL-088217@users.noreply.github.com>
2022-03-07 22:36:42 +0800

    Added frame rate control

--- main.cpp
+++ main.cpp
@@ -6,13 +6,15 @@ int main() {
     DahengCamera camera;
     cv::Mat frames;
     auto ts = (double) cv::getTickCount();
-    if (camera.dahengCameraInit((char *) "KE0210030295", 2500), 5) {
+    if (camera.dahengCameraInit((char *) "KE0210030295", 2500, 225)) {
         while (1) {
             ts = (double) cv::getTickCount();
             frames = camera.getFrame();
             cv::imshow("aaa", frames);
 
             std::cout << 1 / ((cv::getTickCount() - ts) / cv::getTickFrequency()) << std::endl;
+            std::cout << frames.cols << std::endl;
+            std::cout << frames.rows << std::endl;
             cv::waitKey(1);
         }
     }

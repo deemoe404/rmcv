9d314a6ba887321400b17807a0e832d9a12c0fed
EarthOL-088217 <90408077+EarthOL-088217@users.noreply.github.com>
2022-03-25 11:21:02 +0800

    Serial mode added

--- main.cpp
+++ main.cpp
@@ -5,9 +5,10 @@
 // TODO: everything use float, except PnP(double) (needs more investigation)
 
 int main(int argc, char *argv[]) {
-    // Debug mode
+    /// Debug mode
     if (argc == 2 && (*argv[1] == '1' || *argv[1] == '0')) {
         auto camp = *argv[1] == '0' ? rm::CAMP_RED : rm::CAMP_BLUE;
+        int count = 0;
         rm::DahengCamera camera;
         bool cameraStatus = camera.dahengCameraInit((char *) "KE0210010003", 2500, 210);
         while (cameraStatus) {
@@ -23,8 +24,9 @@ int main(int argc, char *argv[]) {
                 // Fit armours
                 std::vector<rm::LightBar> lightBars;
                 std::vector<rm::Armour> armours;
-                rm::FindLightBars(contours, lightBars, 2, 19, 90, 10);
-                rm::FindArmour(lightBars, armours, 20, 8, 0.12, 0.5, 0.65, 0.24, {frame.cols, frame.rows}, camp);
+                rm::FindLightBars(contours, lightBars, 2, 19, 65, 10);
+                rm::FindArmour(lightBars, armours, 20, 8, 0.12, 0.5, 0.65, camp == rm::CAMP_RED ? 0.263 : 0.19,
+                               {frame.cols, frame.rows}, camp);
 
                 if (!armours.empty()) {
                     cv::Mat icon;
@@ -32,21 +34,25 @@ int main(int argc, char *argv[]) {
                     rm::CalcGamma(icon, icon, 0.05);
                     std::vector<cv::Mat> channels;
                     cv::split(icon, channels);
-                    cv::Mat gray = channels[*argv[1] == '1' ? 2 : 0] + channels[1];
+                    cv::Mat gray = channels[*argv[1] == '1' ? 0 : 2];
                     gray.convertTo(gray, CV_32FC1);
-                    cv::imshow("test", gray);
-
-//                    cv::imwrite("/home/yaione/Desktop/sbdum2.jpg", icon);
+                    cv::imshow("icon", gray);
+                    cv::imwrite("/home/yaione/Desktop/bigall/" + rm::int2str(count) + ".jpg", gray);
+                    count++;
                 }
 
                 rm::debug::DrawArmours(armours, frame, -1);
                 rm::debug::DrawLightBars(lightBars, frame, -1);
+                cv::resize(frame, frame, {800, 600});
                 cv::imshow("frame", frame);
                 cv::imshow("binary", binary);
                 cv::waitKey(1);
             }
         }
-    } else if (argc == 2 && *argv[1] == '3') {
+    }
+
+    /// Serial mode
+    if (argc == 2 && *argv[1] == '3') {
         rm::SerialPort serialPort;
         rm::Request request{0, 0, 18, 0};
         bool serialPortStatus = serialPort.Initialize();
@@ -72,70 +78,74 @@ int main(int argc, char *argv[]) {
         while (cameraStatus) {
             cv::Mat frame = camera.getFrame();
             if (!frame.empty()) {
-                // Extract color
-                cv::Mat binary;
-                rm::ExtractColor(frame, binary, static_cast<rm::CampType>(request.camp));
+                cameraStatus = false;
+                continue;
+            }
+            // Extract color
+            cv::Mat binary;
+            rm::ExtractColor(frame, binary, static_cast<rm::CampType>(request.camp));
 
-                // Find contours
-                std::vector<std::vector<cv::Point>> contours;
-                cv::findContours(binary, contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_NONE);
+            // Find contours
+            std::vector<std::vector<cv::Point>> contours;
+            cv::findContours(binary, contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_NONE);
 
-                // Fit armours
-                std::vector<rm::LightBar> lightBars;
-                std::vector<rm::Armour> armours;
-                rm::FindLightBars(contours, lightBars, 2, 19, 65, 10);
-                rm::FindArmour(lightBars, armours, 20, 8, 0.12, 0.5, 0.65, 0.22, {frame.cols, frame.rows},
-                               request.camp == 0 ? rm::CAMP_BLUE : rm::CAMP_RED);
+            // Fit armours
+            std::vector<rm::LightBar> lightBars;
+            std::vector<rm::Armour> armours;
+            rm::FindLightBars(contours, lightBars, 2, 19, 65, 10);
+            rm::FindArmour(lightBars, armours, 20, 8, 0.12, 0.5, 0.65, 0.22, {frame.cols, frame.rows},
+                           request.camp == 0 ? rm::CAMP_BLUE : rm::CAMP_RED);
 
-                for (auto &armour: armours) {
-                    cv::Mat icon;
-                    rm::CalcRatio(frame, icon, armour.icon, armour.iconBox, {30, 30});
-                    rm::CalcGamma(icon, icon, 0.05);
-                    std::vector<cv::Mat> channels;
-                    cv::split(icon, channels);
-                    cv::Mat gray = channels[armour.campType == rm::CAMP_RED ? 0 : 2];
-                    gray.convertTo(gray, CV_32FC1);
-                    Mat cp;
-                    cv::Mat rows = gray.reshape(0, 1);
-                    model->predict(rows, cp);
+            for (auto &armour: armours) {
+                cv::Mat icon;
+                rm::CalcRatio(frame, icon, armour.icon, armour.iconBox, {30, 30});
+                rm::CalcGamma(icon, icon, 0.05);
+                std::vector<cv::Mat> channels;
+                cv::split(icon, channels);
+                cv::Mat gray = channels[armour.campType == rm::CAMP_RED ? 0 : 2];
+                gray.convertTo(gray, CV_32FC1);
+                Mat cp;
+                cv::Mat rows = gray.reshape(0, 1);
+                model->predict(rows, cp);
 
-                    double maxValue = 0;
-                    int maxIndex[2] = {0};
-                    cv::minMaxIdx(cp, nullptr, &maxValue, nullptr, maxIndex);
-                    armour.forceType = maxValue > 0.9 ? static_cast<rm::ForceType>(maxIndex[1]) : rm::FORCE_UNKNOWN;
-                }
+                double maxValue = 0;
+                int maxIndex[2] = {0};
+                cv::minMaxIdx(cp, nullptr, &maxValue, nullptr, maxIndex);
+                armour.forceType = maxValue > 0.9 ? static_cast<rm::ForceType>(maxIndex[1]) : rm::FORCE_UNKNOWN;
+            }
 
-                int index = -1;
-                for (int i = 0; i < armours.size(); i++) {
-                    if (armours[i].forceType == lastTarget) {
-                        index = i;
-                        continue;
-                    } else if (armours[i].forceType != rm::FORCE_UNKNOWN && index < 0) {
-                        index = i;
-                        lastTarget = armours[i].forceType;
-                    }
-                }
-                if (index != -1) {
-                    rm::SolveArmourPose(armours[index], cameraMatrix, distCoeffs, {21.5, 5.5});
-                    rm::SolveAirTrack(armours[index], 9.8, (int) request.speed, 10, request.pitch.data);
-                    response.pitch.data = armours[index].pitch;
-                    response.yaw.data = armours[index].yaw;
-                    response.rank = armours[index].rank;
-                    std::cout << (double) (cv::getTickCount() - time) / cv::getTickFrequency() << "  "
-                              << response.pitch.data << "   " << armours[index].forceType << std::endl;
-                    time = cv::getTickCount();
-
-                    if (!serialPort.Send(response)) {
-                        std::cout << response.pitch.data << std::endl;
-                    }
+            int index = -1;
+            for (int i = 0; i < armours.size(); i++) {
+                if (armours[i].forceType == lastTarget) {
+                    index = i;
+                    continue;
+                } else if (armours[i].forceType != rm::FORCE_UNKNOWN && index < 0) {
+                    index = i;
+                    lastTarget = armours[i].forceType;
                 }
             }
+            if (index != -1) {
+                rm::SolveArmourPose(armours[index], cameraMatrix, distCoeffs, {21.5, 5.5});
+                rm::SolveAirTrack(armours[index], 9.8, (int) request.speed, 10, request.pitch.data);
+                response.pitch.data = armours[index].pitch;
+                response.yaw.data = armours[index].yaw;
+                response.rank = armours[index].rank;
+                std::cout << (double) (cv::getTickCount() - time) / cv::getTickFrequency() << "  "
+                          << response.pitch.data << "   " << armours[index].forceType << std::endl;
+                time = cv::getTickCount();
+
+                if (!serialPort.Send(response)) {
+                    std::cout << response.pitch.data << std::endl;
+                }
+            }
+
         }
         serialPortThread.join();
     }
 
+    /// Concurrent mode
     rm::SerialPort serialPort;
-    rm::Request request{0, 0, 18, 0};
+    rm::Request request{1, 0, 18, 0};
     bool serialPortStatus = serialPort.Initialize();
     // Serial port request receiving thread
     thread serialPortThread([&]() {
@@ -184,8 +194,9 @@ int main(int argc, char *argv[]) {
                 // Fit armours
                 std::vector<rm::LightBar> lightBars;
                 rm::FindLightBars(contours, lightBars, 2, 19, 65, 10);
-                rm::FindArmour(lightBars, result.armours, 20, 8, 0.12, 0.5, 0.65, 0.24,
-                               {package->frame.cols, package->frame.rows}, package->camp);
+                rm::FindArmour(lightBars, result.armours, 20, 8, 0.12, 0.5, 0.65,
+                               package->camp == rm::CAMP_RED ? 0.263 : 0.19, {package->frame.cols, package->frame.rows},
+                               package->camp);
 
                 armourPackageQueue.push(result);
 
@@ -202,6 +213,8 @@ int main(int argc, char *argv[]) {
 
     rm::ParallelQueue<rm::Package> targetQueue;
     Ptr<cv::ml::ANN_MLP> model = cv::ml::ANN_MLP::load("test2.xml");
+    cv::Mat cameraMatrix = (cv::Mat_<double>(3, 3) << 1279.7, 0, 619.4498, 0, 1279.1, 568.4985, 0, 0, 1);
+    cv::Mat distCoeffs = (cv::Mat_<double>(1, 5) << -0.107365897147967, 0.353460341713276, 0, 0, -0.370048735508088);
     // MLP predicting thread
     thread MLPThread([&]() {
         while (frameStatus) {
@@ -230,15 +243,23 @@ int main(int argc, char *argv[]) {
 
                     result.armours[i].forceType =
                             maxValue > 0.9 ? static_cast<rm::ForceType>(maxIndex[1]) : rm::FORCE_UNKNOWN;
+
+                    // ignore if distance farther than around 3m
+                    if (result.armours[i].forceType != rm::FORCE_UNKNOWN) {
+                        rm::SolveArmourPose(result.armours[i], cameraMatrix, distCoeffs,
+                                            (result.armours[i].armourType == rm::ARMOUR_BIG) ? cv::Size2f{21.5, 5.5}
+                                                                                             : cv::Size2f{12.5, 5.5});
+                        if (result.armours[i].tvecs.ptr<double>(0)[2] > 350) {
+                            result.armours[i].forceType = rm::FORCE_UNKNOWN;
+                        }
+                    }
                 }
-            }, cv::getNumThreads());
+            }, cv::getNumberOfCPUs());
             targetQueue.push(result);
         }
     });
 
     rm::Response response{0, 0, 0};
-    cv::Mat cameraMatrix = (cv::Mat_<double>(3, 3) << 1279.7, 0, 619.4498, 0, 1279.1, 568.4985, 0, 0, 1);
-    cv::Mat distCoeffs = (cv::Mat_<double>(1, 5) << -0.107365897147967, 0.353460341713276, 0, 0, -0.370048735508088);
     thread solveThread([&]() {
         int lastTarget = 1;
         while (frameStatus) {
@@ -254,13 +275,13 @@ int main(int argc, char *argv[]) {
                 }
             }
             if (index != -1) {
-                rm::SolveArmourPose(package->armours[index], cameraMatrix, distCoeffs, {21.5, 5.5});
                 rm::SolveAirTrack(package->armours[index], 9.8, package->speed, 10, package->pitch);
                 response.pitch.data = package->armours[index].pitch;
                 response.yaw.data = package->armours[index].yaw;
                 response.rank = package->armours[index].rank;
 
-                std::cout << package->armours[index].tvecs.ptr<double>(0)[2] << std::endl;
+                std::cout << package->armours[index].forceType << "  "
+                          << package->armours[index].tvecs.ptr<double>(0)[2] << std::endl;
 
                 if (!serialPort.Send(response)) {
                     std::cout << response.pitch.data << std::endl;

8a4a65e29e3240abdcf2f1669f7e8581aedca49b
EarthOL-088217 <90408077+EarthOL-088217@users.noreply.github.com>
2022-04-05 22:29:59 +0800

    not treat big/small armour as different anymore

--- src/debug.cpp
+++ src/debug.cpp
@@ -8,33 +8,23 @@ namespace rm::debug {
     void DrawArmours(const std::vector<rm::Armour> &input, cv::Mat &output, int index) {
         if (input.empty()) return;
 
-        std::vector<std::vector<cv::Point>> contoursBig;
-        std::vector<std::vector<cv::Point>> contoursSmall;
+        std::vector<std::vector<cv::Point>> contours;
         if (index < 0 || index > input.size()) {
             for (auto &armour: input) {
                 std::vector<cv::Point> vertices;
                 for (auto &vertex: armour.vertices) {
                     vertices.push_back(vertex);
                 }
-                if (armour.armourType == rm::ARMOUR_BIG) {
-                    contoursBig.push_back(vertices);
-                } else {
-                    contoursSmall.push_back(vertices);
-                }
+                contours.push_back(vertices);
             }
         } else {
             std::vector<cv::Point> vertices;
             for (auto &vertex: input[index].vertices) {
                 vertices.push_back(vertex);
             }
-            if (input[index].armourType == rm::ARMOUR_BIG) {
-                contoursBig.push_back(vertices);
-            } else {
-                contoursSmall.push_back(vertices);
-            }
+            contours.push_back(vertices);
         }
-        cv::drawContours(output, contoursBig, -1, {0, 255, 255}, 1);
-        cv::drawContours(output, contoursSmall, -1, {255, 255, 0}, 1);
+        cv::drawContours(output, contours, -1, {0, 255, 255}, 1);
     }
 
     void DrawLightBars(const vector<rm::LightBar> &input, Mat &output, int index) {

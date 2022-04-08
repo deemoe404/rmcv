796838f529540ef44d2962c498df924f4c345e47
EarthOL-088217 <90408077+EarthOL-088217@users.noreply.github.com>
2022-04-08 17:43:04 +0800

    recovered previous method avoiding fake armour detection

--- include/rmcv/imgproc.h
+++ include/rmcv/imgproc.h
@@ -8,13 +8,42 @@
 #include "rmcv/core/core.h"
 
 namespace rm {
-    void CalcRatio(cv::Mat &input, cv::Mat &output, cv::Point vertices[4], cv::Rect box, cv::Size outSize);
+    /// Calibrate a portion of the source frame to the destine rect.
+    /// \param source Source image.
+    /// \param calibration Output calibrated image.
+    /// \param vertices Vertices of the portion on the source frame.
+    /// \param outSize Destine size.
+    void CalcRatio(cv::Mat &source, cv::Mat &calibration, cv::Point vertices[4], cv::Size outSize);
 
-    void CalcGamma(cv::Mat &input, cv::Mat &output, float gamma = 0.5f);
+    /// Perform a gamma transform on the input image.
+    /// \param source Source image.
+    /// \param calibration Output calibrated image.
+    /// \param gamma Gamma factor.
+    void CalcGamma(cv::Mat &source, cv::Mat &calibration, float gamma = 0.5f);
 
-    void ExtractColor(cv::Mat &input, cv::Mat &output, rm::CampType camp);
+    /// Extract enemy color from source image.
+    /// \param image Source image.
+    /// \param binary Output binary image.
+    /// \param ownCamp Own camp.
+    /// \param overExposed Specify if the image has been over exposed. If true, the color of own camp would be
+    ///                    extracted somehow, consider pass source image to rm::FindLightBars function when finding
+    ///                    light bars.
+    /// \param lowerBound Lower bound when performing binarization.
+    /// \param kernelSize Kernel size when performing dilate.
+    void
+    ExtractColor(cv::InputArray image, cv::OutputArray binary, rm::CampType ownCamp, bool overExposed, int lowerBound,
+                 cv::Size kernelSize);
 
-    void EnhanceIcon();
+    /// Auto enhance image by the given benchmarks.
+    /// \param frame Source image & destine image.
+    /// \param maxGainFactor The mean value of pixel values where gain should be maximize.
+    /// \param minGainFactor The mean value of pixel values where gain should be minimize.
+    void AutoEnhance(cv::Mat &frame, float maxGainFactor = 100.0, float minGainFactor = 50.0);
+
+    /// Use the mean value of each channels to binarize given image and normalize to CV_32FC1.
+    /// \param image Source image.
+    /// \param binary Destine image.
+    void AutoBinarize(cv::Mat &image, cv::Mat &binary);
 }
 
 #endif //RM_STANDARD2022_IMGPROC_H
--- src/imgproc.cpp
+++ src/imgproc.cpp
@@ -5,24 +5,19 @@
 #include "rmcv/imgproc.h"
 
 namespace rm {
-    void CalcRatio(cv::Mat &input, cv::Mat &output, cv::Point vertices[4], cv::Rect box, cv::Size outSize) {
+    void CalcRatio(cv::Mat &source, cv::Mat &calibration, cv::Point vertices[4], cv::Size outSize) {
         // handel the case of vertices out of screen
-        bool overSized = false;
         for (int i = 0; i < 4; i++) {
-            if (vertices[i].x < 0 || vertices[i].y < 0 || vertices[i].x > input.cols - 1 ||
-                vertices[i].y > input.rows - 1) {
+            if (vertices[i].x < 0 || vertices[i].y < 0 || vertices[i].x > source.cols - 1 ||
+                vertices[i].y > source.rows - 1) {
                 if (vertices[i].x < 0) vertices[i].x = 0;
                 if (vertices[i].y < 0) vertices[i].y = 0;
-                if (vertices[i].x > input.cols - 1) vertices[i].x = input.cols - 1;
-                if (vertices[i].y > input.rows - 1) vertices[i].y = input.rows - 1;
-
-                overSized = true;
+                if (vertices[i].x > source.cols - 1) vertices[i].x = source.cols - 1;
+                if (vertices[i].y > source.rows - 1) vertices[i].y = source.rows - 1;
             }
         }
-        if (overSized) {
-            box = cv::boundingRect(std::vector<cv::Point>({vertices[0], vertices[1], vertices[2], vertices[3]}));
-        }
 
+        cv::Rect box = cv::boundingRect(std::vector<cv::Point>({vertices[0], vertices[1], vertices[2], vertices[3]}));
         cv::Point2f srcPts[3] = {{(float) (vertices[1].x - box.x), (float) (vertices[1].y - box.y)},
                                  {(float) (vertices[2].x - box.x), (float) (vertices[2].y - box.y)},
                                  {(float) (vertices[0].x - box.x), (float) (vertices[0].y - box.y)}};
@@ -30,13 +25,13 @@ namespace rm {
                                  {(float) (box.width), 0},
                                  {0,                   (float) (box.height)}};
         auto warp = cv::getAffineTransform(srcPts, dstPts);
-        cv::warpAffine(input(box), output, warp, output.size());
+        cv::warpAffine(source(box), calibration, warp, calibration.size());
 
         // resize to the dst size
-        cv::resize(output, output, outSize);
+        cv::resize(calibration, calibration, outSize);
     }
 
-    void CalcGamma(cv::Mat &input, cv::Mat &output, float gamma) {
+    void CalcGamma(cv::Mat &source, cv::Mat &calibration, float gamma) {
         cv::Mat lookUpTable(1, 256, CV_8U);
         uchar *p = lookUpTable.ptr();
         for (int i = 0; i < 256; ++i) {
@@ -44,19 +39,55 @@ namespace rm {
         }
 
         // search for LUT table
-        cv::LUT(input, lookUpTable, output);
+        cv::LUT(source, lookUpTable, calibration);
     }
 
-    void ExtractColor(cv::Mat &input, cv::Mat &output, rm::CampType camp) {
-        std::vector<cv::Mat> channels;
-        cv::split(input, channels);
+    void
+    ExtractColor(cv::InputArray image, cv::OutputArray binary, rm::CampType ownCamp, bool overExposed, int lowerBound,
+                 cv::Size kernelSize) {
+        if (overExposed) {
+            cv::inRange(image, ownCamp == rm::CAMP_RED ? cv::Scalar{250, (double) lowerBound, (double) lowerBound}
+                                                       : cv::Scalar{(double) lowerBound, (double) lowerBound, 250},
+                        cv::Scalar{255, 255, 255}, binary);
+        } else {
+            std::vector<cv::Mat> channels;
+            cv::split(image, channels);
 
-        // extract color
-        auto gray = channels[camp == rm::CAMP_RED ? 0 : 2] - channels[camp == rm::CAMP_RED ? 2 : 0];
-        cv::inRange(gray, 65, 255, output);
+            // extract color
+            auto gray = channels[ownCamp == rm::CAMP_RED ? 0 : 2] - channels[ownCamp == rm::CAMP_RED ? 2 : 0];
+            cv::inRange(gray, lowerBound, 255, binary);
+        }
 
         // enhance visual
-        auto kernel = cv::getStructuringElement(cv::MORPH_ELLIPSE, {3, 3});
-        cv::morphologyEx(output, output, cv::MORPH_CLOSE, kernel);
+        if (kernelSize.width != 0 && kernelSize.height != 0) {
+            auto kernel = cv::getStructuringElement(cv::MORPH_RECT, {kernelSize.width, kernelSize.height});
+            cv::morphologyEx(binary, binary, cv::MORPH_DILATE, kernel);
+        }
+    }
+
+    void AutoEnhance(cv::Mat &frame, float maxGainFactor, float minGainFactor) {
+        cv::Scalar meanValue = cv::mean(frame);
+        float meanC3 = (float) (meanValue[0] + meanValue[1] + meanValue[2]) / 3.0f;
+        float k = 2.0f / (maxGainFactor - minGainFactor);
+        float b = 3.0f - maxGainFactor * k;
+        float gammaFactor = k * meanC3 + b;
+
+        // Map [1.0, -3.0] to [1.0, 0.0]
+        if (gammaFactor <= 1.0f && gammaFactor >= -3.0f) {
+            gammaFactor = 1.0f + (gammaFactor - 1) / 4.0f;
+        } else if (gammaFactor < -3.0f) {
+            // Frame too dark, gamma might not helpful in this case.
+            gammaFactor = 0;
+        }
+
+        rm::CalcGamma(frame, frame, gammaFactor);
+    }
+
+    void AutoBinarize(cv::Mat &image, cv::Mat &binary) {
+        cv::cvtColor(image, binary, cv::COLOR_BGR2GRAY);
+        cv::Scalar meanValue = cv::mean(binary);
+        cv::inRange(binary, cv::Scalar{meanValue[0], meanValue[1], meanValue[2]}, cv::Scalar{255, 255, 255}, binary);
+
+        binary.convertTo(binary, CV_32FC1, 1.0 / 255.0);
     }
 }

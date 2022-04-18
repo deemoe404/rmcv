e637888e01f818f9792438b08577ccc886b1c40d
EarthOL-088217 <90408077+EarthOL-088217@users.noreply.github.com>
2022-04-18 17:11:49 +0800

    updated serialport.cpp module

--- src/serialport.cpp
+++ src/serialport.cpp
@@ -5,24 +5,24 @@
 #include "rmcv/core/serialport.h"
 
 namespace rm {
-    uc LookupCRC(uc *data, uc dataLen) {
+    unsigned char LookupCRC(unsigned char *data, unsigned char dataLength, const unsigned char *crcTable) {
         uint8_t crc = 0x00;
 
-        while (dataLen--) {
-            crc = CRCTable[crc ^ *data++];
+        while (dataLength--) {
+            crc = crcTable[crc ^ *data++];
         }
         return crc;
     }
 
-    bool SerialPort::Initialize(const char *device) {
+    bool SerialPort::Initialize(const char *device, unsigned int speed) {
         fd = open(device, O_RDWR | O_NOCTTY | O_NDELAY);
         if (fd != -1) {
             fcntl(fd, F_SETFL, 0);
 
             struct termios options{};
             if (tcgetattr(fd, &options) == 0) {
-                cfsetospeed(&options, B460800);
-                cfsetispeed(&options, B460800);
+                cfsetospeed(&options, speed);
+                cfsetispeed(&options, speed);
 
                 options.c_cflag |= (CLOCAL | CREAD);
                 options.c_cflag &= ~CRTSCTS;
@@ -34,35 +34,25 @@ namespace rm {
                 options.c_lflag = 0;
 
                 tcflush(fd, TCIFLUSH);
-                return !tcsetattr(fd, TCSANOW, &options);
+
+                if (!tcsetattr(fd, TCSANOW, &options)) {
+                    initialized = true;
+                    return true;
+                }
             }
         }
         return false;
     }
 
-    bool SerialPort::Send(Response &message) {
-        //check
-        sendBuffer[0] = 0x66;
-
-        //pitch
-        sendBuffer[1] = message.pitch.bit[0];
-        sendBuffer[2] = message.pitch.bit[1];
-        sendBuffer[3] = message.pitch.bit[2];
-        sendBuffer[4] = message.pitch.bit[3];
-
-        //yaw
-        sendBuffer[5] = message.yaw.bit[0];
-        sendBuffer[6] = message.yaw.bit[1];
-        sendBuffer[7] = message.yaw.bit[2];
-        sendBuffer[8] = message.yaw.bit[3];
-
-        //rank
-        sendBuffer[9] = message.rank;
-
-        return write(fd, sendBuffer, 10) > 0;
+    bool SerialPort::Send(const unsigned char *packet, int length) const {
+        if (initialized) {
+            return write(fd, packet, length) > 0;
+        } else {
+            throw std::runtime_error("Serial port not initialized.");
+        }
     }
 
-    bool SerialPort::Receive(Request &message) {
+    bool SerialPort::Receive(unsigned char *packet, int length) {
         fd_set fdRead;
         FD_ZERO(&fdRead);
         FD_SET(fd, &fdRead);
@@ -79,29 +69,7 @@ namespace rm {
             return false;
         }
 
-        if (read(fd, receiveBuffer, 8) == 8) {
-            if (receiveBuffer[0] != 0x38) {
-                return false;
-            }
-
-            if (receiveBuffer[7] != LookupCRC(receiveBuffer, 7)) {
-                return false;
-            }
-
-            // TODO: confirm the definition of camp & mode.
-            message.ownCamp = receiveBuffer[1] & 0x01;
-            message.mode = (receiveBuffer[1] & 0x04) >> 2;
-            message.speed = receiveBuffer[2];
-
-            message.pitch.bit[0] = receiveBuffer[3];
-            message.pitch.bit[1] = receiveBuffer[4];
-            message.pitch.bit[2] = receiveBuffer[5];
-            message.pitch.bit[3] = receiveBuffer[6];
-
-            return true;
-        } else {
-            return false;
-        }
+        return read(fd, packet, length) == length;
     }
 
     void SerialPort::Destroyed() const {

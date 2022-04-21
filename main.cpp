9bc115119f8be2577849e885a1e955451f68b280
EarthOL-088217 <90408077+EarthOL-088217@users.noreply.github.com>
2022-04-21 22:54:06 +0800

    updated demo

--- main.cpp
+++ main.cpp
@@ -4,70 +4,136 @@
 
 // TODO: everything use float, except PnP(double) (needs more investigation)
 
-int main(int argc, char *argv[]) {
-    rm::SerialPort serialPort;
+class Request {
+public:
+    rm::CampType OwnCamp;
+    rm::AimMode Mode;
+    int FireRate;
+    float GimbalPitch;
 
-    rm::Request request{1, 0, 18, 0};
-    thread serialPortThread([&]() {
-        bool status = serialPort.Initialize();
-        int failure = 0;
+    Request() = default;
+
+    Request(rm::CampType camp, rm::AimMode mode) : OwnCamp(camp), Mode(mode), FireRate(30), GimbalPitch(0) {}
+};
+
+class Response {
+public:
+    float Pitch;
+    float Yaw;
+    unsigned char Rank;
+
+    Response() = default;
+
+    Response(float pitch, float yaw, unsigned char rank) : Pitch(pitch), Yaw(yaw), Rank(rank) {};
+};
+
+int main(int argc, char *argv[]) {
+    Request request(rm::CAMP_BLUE, rm::AIM_COMBAT);
+
+    rm::SerialPort serialPort;
+    bool status = serialPort.Initialize("/dev/ttyUSB0", B460800);
+    thread readThread([&]() {
         while (status) {
-            if (!serialPort.Receive(request) && failure < 256) {
+            unsigned char readBuff[8];
+
+            if (!serialPort.Receive(readBuff, 8)) {
                 std::cout << "Serial port receive failed." << std::endl;
-                failure++;
-            } else {
-                status = false;
+                continue;
             }
+
+            if (readBuff[0] != 0x38) {
+                std::cout << "Head error." << std::endl;
+                continue;
+            }
+
+            if (readBuff[7] != rm::LookupCRC(readBuff, 7)) {
+                std::cout << "CRC error." << std::endl;
+                continue;
+            }
+
+            request.OwnCamp = static_cast<rm::CampType>(readBuff[1] & 0x01);
+            request.Mode = static_cast<rm::AimMode>((readBuff[1] & 0x04) >> 2);
+            request.FireRate = (int) readBuff[2];
+
+            std::memcpy(&request.GimbalPitch, readBuff + 3, sizeof(float));
+
+            std::cout << request.GimbalPitch << std::endl;
         }
-        std::cout << "Serial port closed." << std::endl;
+        std::cout << "Serial port read closed." << std::endl;
     });
 
-    cv::Mat cameraMatrix = (cv::Mat_<double>(3, 3) << 1279.7, 0, 619.4498, 0, 1279.1, 568.4985, 0, 0, 1);
-    cv::Mat distCoeffs = (cv::Mat_<double>(1, 5) << -0.107365897147967, 0.353460341713276, 0, 0, -0.370048735508088);
-    thread detectionThread([&]() {
-        rm::DahengCamera camera;
-        bool status = camera.dahengCameraInit((char *) "KE0210030295", false, (int) (1.0 / 210.0 * 1000000), 0.0);
+    rm::ParallelQueue<Response> msgs;
+    thread sendThread([&]() {
+        shared_ptr<Response> msg;
+        unsigned char sendBuff[11];
         while (status) {
-            cv::Mat frame = camera.getFrame();
-            auto ownCamp = static_cast<rm::CampType>(request.ownCamp);
+            msg = msgs.pop();
+
+            sendBuff[0] = 0x66;
+            std::memcpy(sendBuff + 1, &msg->Pitch, sizeof(float));
+            std::memcpy(sendBuff + 5, &msg->Yaw, sizeof(float));
+            sendBuff[9] = msg->Rank;
+            sendBuff[10] = rm::LookupCRC(sendBuff, 10);
+            if (!serialPort.Send(sendBuff, 11)) {
+                std::cout << "Send failed" << std::endl;
+            }
+        }
+        std::cout << "Serial port send closed." << std::endl;
+    });
+
+    long tick = cv::getTickCount();
+    thread detectionThread([&]() {
+        cv::Mat cameraMatrix = (cv::Mat_<double>(3, 3) << 1279.7, 0, 619.4498, 0, 1279.1, 568.4985, 0, 0, 1);
+        cv::Mat distCoeffs = (cv::Mat_<double>(1, 5)
+                << -0.107365897147967, 0.353460341713276, 0, 0, -0.370048735508088);
+
+        rm::ShootFactor result;
+        cv::Mat frame, binary, tvecs, rvecs;
+        std::vector<rm::LightBar> lightBars(1024);
+        std::vector<rm::Armour> armours(512);
+
+        rm::DahengCamera camera;
+        bool status = camera.dahengCameraInit((char *) "KE0210030295", false, (int) (1.0 / 210.0 * 1000000), 0.5);
+        while (status) {
+            frame = camera.getFrame();
+            tick = cv::getTickCount();
             if (!frame.empty()) {
-                cv::Mat binary;
-                rm::ExtractColor(frame, binary, ownCamp, true, 40, {5, 5});
-
-                std::vector<rm::LightBar> lightBars;
+                rm::ExtractColor(frame, binary, request.OwnCamp, true, 40, {5, 5});
                 rm::FindLightBars(binary, lightBars, 2, 10, 25, 80, 1500, frame, true);
-
-                std::vector<rm::Armour> armours;
-                rm::FindArmour(lightBars, armours, 8, 24, 0.15, 0.45, 0.65, ownCamp,
+                rm::FindArmour(lightBars, armours, 8, 24, 0.15, 0.45, 0.65, request.OwnCamp,
                                cv::Size2f{(float) frame.cols, (float) frame.rows});
 
                 for (auto &armour: armours) {
-                    cv::Mat tvecs, rvecs;
                     rm::SolvePNP(armour.vertices, cameraMatrix, distCoeffs, {5.5, 5.5}, tvecs, rvecs);
-                    double distance = rm::SolveDistance(tvecs);
-                    if (distance < 450) {
-                        rm::SolveDeltaHeight(tvecs, request.pitch.data);
-                        rm::ShootFactor shoot;
-                        rm::SolveShootFactor(tvecs, shoot, 9.8, request.speed, 28, {0, 0}, rm::COMPENSATE_CLASSIC);
-                        rm::Response response;
-                        response.pitch.data = shoot.pitchAngle;
-                        response.yaw.data = shoot.yawAngle;
-                        std::cout << response.pitch.data << std::endl;
+                    if (rm::SolveDistance(tvecs) < 450) {
+                        rm::SolveDeltaHeight(tvecs, request.GimbalPitch);
+                        rm::SolveShootFactor(tvecs, result, 9.8, request.FireRate, -60, {0, 0}, rm::COMPENSATE_CLASSIC);
+
+                        std::cout << "pitch: " << result.pitchAngle << "  yaw: " << result.yawAngle << "  distance:"
+                                  << rm::SolveDistance(tvecs) << "  ";
+                        msgs.push({result.pitchAngle, result.yawAngle, 0});
+                        continue;
                     }
                 }
+                std::cout << std::setiosflags(ios::fixed) << std::setiosflags(ios::right) << std::setprecision(6)
+                          << armours.size() << ": "
+                          << ((double) (cv::getTickCount() - tick) / cv::getTickFrequency()) * 1000.0 << "ms"
+                          << std::endl;
 
-                rm::debug::DrawLightBars(lightBars, frame, -1);
-                rm::debug::DrawArmours(armours, frame, -1);
-                cv::imshow("frame", frame);
-                cv::imshow("binary", binary);
-                cv::waitKey(1);
+//                rm::debug::DrawLightBars(lightBars, frame, -1);
+//                rm::debug::DrawArmours(armours, frame, -1);
+//                cv::imshow("frame", frame);
+//                cv::imshow("binary", binary);
+//                cv::waitKey(1);
             } else {
                 status = false;
             }
         }
     });
 
-    serialPortThread.join();
+
+    readThread.join();
+    sendThread.join();
     detectionThread.join();
     return 0;
 }

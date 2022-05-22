4ccc161e74d5103c8c0bc051f246f4d334ce4148
EarthOL-088217 <90408077+EarthOL-088217@users.noreply.github.com>
2022-05-22 14:58:59 +0800

    renamed class LightBar to LightBlob since guide light isn't a 'bar'

--- include/rmcv/objdetect.h
+++ include/rmcv/objdetect.h
@@ -9,95 +9,69 @@
 
 namespace rm {
     enum CompensateMode {
-        COMPENSATE_NONE = 0,    // No compensate
-        COMPENSATE_CLASSIC = 1, // Use rm::NewtonIteration & rm::ProjectileMotionFD for compensate
-        COMPENSATE_NI = 2       // Use classic Newton's laws of motion for compensate
+        // No compensation
+        COMPENSATE_NONE = 0,
+
+        // Use rm::NewtonIteration & rm::ProjectileMotionFD for compensate
+        COMPENSATE_CLASSIC = 1,
+
+        // Use classic Newton's laws of motion for compensate
+        COMPENSATE_NI = 2
     };
 
-    /// Judge a contour if it's a light blob with the given qualifications.
-    /// \param contour       The contour.
-    /// \param minRatio      Minimal aspect ratio.
-    /// \param maxRatio      Maximal aspect ratio.
-    /// \param tiltAngle     Maximal tilt angle.
-    /// \param minArea       Minimal contour area.
-    /// \param maxArea       Maximal contour area.
-    /// \param useFitEllipse Use cv::fitEllipseDirect to define light blob outlines instead of cv::minAreaRect when set
-    ///                      to true. Usually recommended when source frame was over exposed.
-    bool
-    JudgeLightBlob(const std::vector<cv::Point> &contour, float minRatio, float maxRatio, float tilAngle, float minArea,
-                   float maxArea, bool useFitEllipse = true);
+    /// Match a contour if it's a light blob with the given condition.
+    /// \param contour    Input contour (more than 6 points).
+    /// \param minRatio   Minimal aspect ratio.
+    /// \param maxRatio   Maximal aspect ratio.
+    /// \param tiltAngle  Maximal tilt angle.
+    /// \param minArea    Minimal contour area.
+    /// \param maxArea    Maximal contour area.
+    /// \param fitEllipse Use cv::fitEllipseDirect to define light blob outlines instead of cv::minAreaRect when set
+    ///                   to true. Usually recommended when source frame was over exposed.
+    /// \return True if condition matches.
+    bool MatchLightBlob(const rm::Contour &contour, float minRatio, float maxRatio, float tiltAngle, float minArea,
+                        float maxArea, cv::RotatedRect &lightBlobBox, bool fitEllipse = true);
 
-    /// Fit light blobs from a set of contours with the given color. (Each contours must contains more than 6 points.)
-    /// \param contours      The set of contours.
-    /// \param lightBlobs     Output light blobs.
-    /// \param minRatio      Minimal aspect ratio.
-    /// \param maxRatio      Maximal aspect ratio.
-    /// \param tiltAngle     Maximal tilt angle.
-    /// \param minArea       Minimal contour area.
-    /// \param maxArea       Maximal contour area.
-    /// \param frame         Source frame. Used for auto color detection.
-    /// \param useFitEllipse Use cv::fitEllipseDirect to define light blob outlines instead of cv::minAreaRect when set
-    ///                      to true. Usually recommended when source frame was over exposed.
-    void
-    FindLightBlobs(std::vector<std::vector<cv::Point>> &contours, std::vector<rm::LightBlob> &lightBlobs, float minRatio,
-                   float maxRatio, float tiltAngle, float minArea, float maxArea, rm::CampType camp = rm::CAMP_RED,
-                   bool useFitEllipse = true);
+    /// Find light blobs from a set of contours. Auto detect light blob camp.
+    /// \param contours   Input contour (more than 6 points).
+    /// \param lightBlobs Output light blobs.
+    /// \param minRatio   Minimal aspect ratio.
+    /// \param maxRatio   Maximal aspect ratio.
+    /// \param tiltAngle  Maximal tilt angle.
+    /// \param minArea    Minimal contour area.
+    /// \param maxArea    Maximal contour area.
+    /// \param source     Source image (3 channels RGB).
+    /// \param fitEllipse Use cv::fitEllipseDirect to box light blob instead of cv::minAreaRect when True.
+    void FindLightBlobs(std::vector<rm::Contour> &contours, std::vector<rm::LightBlob> &lightBlobs, float minRatio,
+                        float maxRatio, float tiltAngle, float minArea, float maxArea, const cv::Mat &source,
+                        bool fitEllipse = true);
 
-    /// Fit light blobs from a set of contours. Auto detect light blob color. (Each contours must contains more than 6
-    /// points.)
-    /// \param contours      The set of contours.
-    /// \param lightBlobs     Output light blobs.
-    /// \param minRatio      Minimal aspect ratio.
-    /// \param maxRatio      Maximal aspect ratio.
-    /// \param tiltAngle     Maximal tilt angle.
-    /// \param minArea       Minimal contour area.
-    /// \param maxArea       Maximal contour area.
-    /// \param frame         Source frame. Used for auto color detection.
-    /// \param useFitEllipse Use cv::fitEllipseDirect to define light blob outlines instead of cv::minAreaRect when set
-    ///                      to true. Usually recommended when source frame was over exposed.
-    void
-    FindLightBlobs(std::vector<std::vector<cv::Point>> &contours, std::vector<rm::LightBlob> &lightBlobs, float minRatio,
-                   float maxRatio, float tiltAngle, float minArea, float maxArea, cv::Mat &frame,
-                   bool useFitEllipse = true);
+    /// Find light blobs from a set of contours.
+    /// \param contours   Input contour (more than 6 points).
+    /// \param lightBlobs Output light blobs.
+    /// \param minRatio   Minimal aspect ratio.
+    /// \param maxRatio   Maximal aspect ratio.
+    /// \param tiltAngle  Maximal tilt angle.
+    /// \param minArea    Minimal contour area.
+    /// \param maxArea    Maximal contour area.
+    /// \param camp       Light blobs camp.
+    /// \param fitEllipse Use cv::fitEllipseDirect to box light blob instead of cv::minAreaRect when True.
+    void FindLightBlobs(std::vector<rm::Contour> &contours, std::vector<rm::LightBlob> &lightBlobs, float minRatio,
+                        float maxRatio, float tiltAngle, float minArea, float maxArea, rm::CampType camp,
+                        bool fitEllipse = true);
 
-    /// Fit light blobs from a binary image. Auto detect light blob color. (Each contours must contains more than 6
-    /// points.)
-    /// \param binary        Source binary image. This function would consider the external contours only.
-    /// \param lightBlobs     Output light blobs.
-    /// \param minRatio      Minimal aspect ratio.
-    /// \param maxRatio      Maximal aspect ratio.
-    /// \param tiltAngle     Maximal tilt angle.
-    /// \param minArea       Minimal contour area.
-    /// \param maxArea       Maximal contour area.
-    /// \param frame         Source frame. Used for auto color detection.
-    /// \param useFitEllipse Use cv::fitEllipseDirect to define light blob outlines instead of cv::minAreaRect when set
-    ///                      to true. Usually recommended when source frame was over exposed.
-    void FindLightBlobs(cv::Mat &binary, std::vector<rm::LightBlob> &lightBlobs, float minRatio, float maxRatio,
-                        float tiltAngle, float minArea, float maxArea, cv::Mat &frame, bool useFitEllipse = true);
+    /// Find guid light
+    /// \param lightBlobs
+    /// \param source
+    /// \return
+    int FindGuidLight(const std::vector<rm::LightBlob> &lightBlobs, const cv::Mat &source);
 
-    /// Detect if there is a interference over all light blobs at the given pair of light blobs.
-    /// \param lightBlobs  All light blobs on the frame.
+    /// Detect if there is a overlap over all light blobs at the given pair of light blobs.
+    /// \param lightBlobs All light blobs on the frame (must be sorted ascending by x).
     /// \param leftIndex  Index of the left light blob.
     /// \param rightIndex Index of the right light blob.
-    /// \return True if there is a interference;
-    bool LightBlobInterference(std::vector<rm::LightBlob> &lightBlobs, int leftIndex, int rightIndex);
-
-    /// Fit armours from a set of light blobs.
-    /// \param lightBlobs   The set of light blobs
-    /// \param armours     Output armours
-    /// \param maxAngleDif Maximum angle difference between two light blobs.
-    /// \param errAngle    Maximum angle between the over all rect and the two light blobs.
-    /// \param minBoxRatio Minimum ratio of the armour box.
-    /// \param maxBoxRatio Maximum ratio of the armour box.
-    /// \param lenRatio    Maximum length ratio between two light blobs.
-    /// \param ownCamp     Own camp.
-    /// \param frameSize   Frame size. When this parameter is specified, the distance between armour and the frame
-    ///                    center would be used to sort the output.
-    /// \param filter      Exclude one if two armours sharing a same light blob, exclude the armour witch minimum height
-    ///                    along two light blobs is smaller.
-    void FindArmour(std::vector<rm::LightBlob> &lightBlobs, std::vector<rm::Armour> &armours, float maxAngleDif,
-                    float errAngle, float minBoxRatio, float maxBoxRatio, float lenRatio, rm::CampType ownCamp,
-                    cv::Size2f frameSize, bool filter = true);
+    /// \return True if there is a overlap;
+    bool LightBlobOverlap(std::vector<rm::LightBlob> &lightBlobs, int leftIndex, int rightIndex);
 
     /// Fit armours from a set of light blobs.
     /// \param lightBlobs   The set of light blobs
@@ -114,7 +88,7 @@ namespace rm {
     ///                    along two light blobs is smaller.
     void FindArmour(std::vector<rm::LightBlob> &lightBlobs, std::vector<rm::Armour> &armours, float maxAngleDif,
                     float errAngle, float minBoxRatio, float maxBoxRatio, float lenRatio, rm::CampType ownCamp,
-                    cv::Point2f attention, bool filter = true);
+                    cv::Point2f attention = {-1.0, -1.0}, bool filter = true);
 
     /// Calculate the shoot factor using Newton Iteration.
     /// \param target      The target armour.
@@ -125,7 +99,12 @@ namespace rm {
     /// \param deltaHeight Height difference between barrel and target (cm).
     void
     SolveShootFactor(cv::Mat &translationVector, rm::ShootFactor &shootFactor, double g, double v0, double deltaHeight,
-                     cv::Point2f offset = {0, 0}, rm::CompensateMode mode = rm::COMPENSATE_NONE);
+                     cv::Point2f offset = {0, 0}, double angleOffset = 0,
+                     rm::CompensateMode mode = rm::COMPENSATE_NONE);
+
+    void
+    SolveShootFactor(rm::ShootFactor &shootFactor, double d, double g, double v0, double h, cv::Point2f offset = {0, 0},
+                     double angleOffset = 0, rm::CompensateMode mode = rm::COMPENSATE_NONE);
 
     void SolveCameraPose(cv::Mat &rvecs, cv::Mat &tvecs, cv::Mat &output);
 }
--- src/objdetect.cpp
+++ src/objdetect.cpp
@@ -5,77 +5,74 @@
 #include "rmcv/objdetect.h"
 
 namespace rm {
-    bool
-    JudgeLightBlob(const std::vector<cv::Point> &contour, float minRatio, float maxRatio, float tilAngle, float minArea,
-                   float maxArea, bool useFitEllipse) {
+    bool MatchLightBlob(const std::vector<cv::Point> &contour, float minRatio, float maxRatio, float tiltAngle,
+                        float minArea, float maxArea, cv::RotatedRect &lightBlobBox, bool fitEllipse) {
         if (contour.size() < 6 || cv::contourArea(contour) < minArea || cv::contourArea(contour) > maxArea)
             return false;
 
-        cv::RotatedRect ellipse = cv::fitEllipseDirect(contour);
-        cv::RotatedRect box = useFitEllipse ? ellipse : cv::minAreaRect(contour);
+        cv::RotatedRect ellipse = cv::fitEllipseDirect(contour); // Calculate tilt angle using ellipse anyway
+        cv::RotatedRect box = fitEllipse ? ellipse : cv::minAreaRect(contour);
 
         // Aspect ratio
         float ratio = std::max(box.size.width, box.size.height) / std::min(box.size.width, box.size.height);
         if (ratio > maxRatio || ratio < minRatio) return false;
 
-        // Angle (Perpendicular to the frame is considered as 90 degrees, left < 90, right > 90)
+        // Tilt angle (Perpendicular to the frame is considered as 90 degrees, left < 90, right > 90)
         float angle = ellipse.angle > 90 ? ellipse.angle - 90 : ellipse.angle + 90;
-        if (abs(angle - 90) > tilAngle) return false;
+        if (abs(angle - 90) > tiltAngle) return false;
 
+        lightBlobBox = box;
         return true;
     }
 
-    void
-    FindLightBlobs(std::vector<std::vector<cv::Point>> &contours, std::vector<rm::LightBlob> &lightBlobs, float minRatio,
-                   float maxRatio, float tiltAngle, float minArea, float maxArea, rm::CampType camp,
-                   bool useFitEllipse) {
-        if (contours.size() < 2) return;
+    void FindLightBlobs(std::vector<rm::Contour> &contours, std::vector<rm::LightBlob> &lightBlobs, float minRatio,
+                        float maxRatio, float tiltAngle, float minArea, float maxArea, const cv::Mat &source,
+                        bool fitEllipse) {
         lightBlobs.clear();
+        if (source.channels() != 3) return;
 
+        cv::RotatedRect box;
         for (auto &contour: contours) {
-            if (!rm::JudgeLightBlob(contour, minRatio, maxRatio, tiltAngle, minArea, maxArea)) continue;
+            if (!rm::MatchLightBlob(contour, minRatio, maxRatio, tiltAngle, minArea, maxArea, box, fitEllipse))
+                continue;
 
-            lightBlobs.emplace_back(useFitEllipse ? cv::fitEllipseDirect(contour) : cv::minAreaRect(contour), camp);
-        }
-    }
-
-    void FindLightBlobs(vector<std::vector<cv::Point>> &contours, vector<rm::LightBlob> &lightBlobs, float minRatio,
-                        float maxRatio, float tiltAngle, float minArea, float maxArea, cv::Mat &frame,
-                        bool useFitEllipse) {
-        lightBlobs.clear();
-
-        for (auto &contour: contours) {
-            if (!rm::JudgeLightBlob(contour, minRatio, maxRatio, tiltAngle, minArea, maxArea)) continue;
-
-            cv::Scalar meanValue = cv::mean(frame(cv::boundingRect(contour)));
-
+            cv::Scalar meanValue = cv::mean(source(cv::boundingRect(contour)));
             if (meanValue.val[1] > meanValue.val[0] && meanValue.val[1] > meanValue.val[2]) {
-                lightBlobs.emplace_back(useFitEllipse ? cv::fitEllipseDirect(contour) : cv::minAreaRect(contour),
-                                       rm::CAMP_OUTPOST);
+                lightBlobs.emplace_back(box, rm::CAMP_OUTPOST);
             } else {
-                lightBlobs.emplace_back(useFitEllipse ? cv::fitEllipseDirect(contour) : cv::minAreaRect(contour),
-                                       meanValue.val[0] > meanValue.val[2] ? rm::CAMP_BLUE : rm::CAMP_RED);
+                lightBlobs.emplace_back(box, meanValue.val[0] > meanValue.val[2] ? rm::CAMP_BLUE : rm::CAMP_RED);
             }
         }
     }
 
-    void FindLightBlobs(cv::Mat &binary, std::vector<rm::LightBlob> &lightBlobs, float minRatio, float maxRatio,
-                        float tiltAngle, float minArea, float maxArea, cv::Mat &frame, bool useFitEllipse) {
-        std::vector<std::vector<cv::Point>> contours;
-        cv::findContours(binary, contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_NONE);
+    void FindLightBlobs(std::vector<rm::Contour> &contours, std::vector<rm::LightBlob> &lightBlobs, float minRatio,
+                        float maxRatio, float tiltAngle, float minArea, float maxArea, rm::CampType camp,
+                        bool fitEllipse) {
+        lightBlobs.clear();
 
-        rm::FindLightBlobs(contours, lightBlobs, minRatio, maxRatio, tiltAngle, minArea, maxArea, frame, useFitEllipse);
+        cv::RotatedRect box;
+        for (auto &contour: contours) {
+            if (!rm::MatchLightBlob(contour, minRatio, maxRatio, tiltAngle, minArea, maxArea, box, fitEllipse))
+                continue;
+
+            lightBlobs.emplace_back(box, camp);
+        }
     }
 
-    bool LightBlobInterference(std::vector<rm::LightBlob> &lightBlobs, int leftIndex, int rightIndex) {
-        for (int k = 0; k < lightBlobs.size(); k++) {
-            if (k == leftIndex && k == rightIndex) continue;
-            if (lightBlobs[k].center.x > min(lightBlobs[leftIndex].center.x, lightBlobs[rightIndex].center.x) &&
-                lightBlobs[k].center.x < max(lightBlobs[leftIndex].center.x, lightBlobs[rightIndex].center.x) &&
-                lightBlobs[k].center.y >
-                min((float) lightBlobs[leftIndex].vertices[1].y, (float) lightBlobs[rightIndex].vertices[1].y) &&
-                lightBlobs[k].center.y <
-                max((float) lightBlobs[leftIndex].vertices[0].y, (float) lightBlobs[rightIndex].vertices[0].y)) {
+    bool LightBlobOverlap(std::vector<rm::LightBlob> &lightBlobs, int leftIndex, int rightIndex) {
+        if (leftIndex < 0 || rightIndex > lightBlobs.size() || rightIndex - leftIndex < 2) return false;
+
+        float lowerY = min(
+                min((float) lightBlobs[leftIndex].vertices[1].y, (float) lightBlobs[leftIndex].vertices[2].y),
+                min((float) lightBlobs[rightIndex].vertices[1].y, (float) lightBlobs[rightIndex].vertices[2].y));
+        float UpperY = max(
+                max((float) lightBlobs[leftIndex].vertices[0].y, (float) lightBlobs[leftIndex].vertices[3].y),
+                max((float) lightBlobs[rightIndex].vertices[0].y, (float) lightBlobs[rightIndex].vertices[3].y));
+
+        for (int i = leftIndex; i < rightIndex; i++) {
+            if (lightBlobs[i].center.x > lightBlobs[leftIndex].center.x &&
+                lightBlobs[i].center.x < lightBlobs[rightIndex].center.x && lightBlobs[i].center.y > lowerY &&
+                lightBlobs[i].center.y < UpperY) {
                 return true;
             }
         }
@@ -84,32 +81,24 @@ namespace rm {
 
     void FindArmour(std::vector<rm::LightBlob> &lightBlobs, std::vector<rm::Armour> &armours, float maxAngleDif,
                     float errAngle, float minBoxRatio, float maxBoxRatio, float lenRatio, rm::CampType ownCamp,
-                    cv::Size2f frameSize, bool filter) {
-        cv::Point2f center = {frameSize.width / 2.0f, frameSize.height / 2.0f};
-
-        rm::FindArmour(lightBlobs, armours, maxAngleDif, errAngle, minBoxRatio, maxBoxRatio, lenRatio, ownCamp, center,
-                       filter);
-    }
-
-    void FindArmour(std::vector<rm::LightBlob> &lightBlobs, std::vector<rm::Armour> &armours, float maxAngleDif,
-                    float errAngle, float minBoxRatio, float maxBoxRatio, float lenRatio, rm::CampType ownCamp,
                     cv::Point2f attention, bool filter) {
         armours.clear();
         if (lightBlobs.size() < 2) return;
 
-        // sort vertices ascending by x
-        std::sort(lightBlobs.begin(), lightBlobs.end(), [](const rm::LightBlob &lightBar1, const rm::LightBlob &lightBar2) {
-            return lightBar1.center.x < lightBar2.center.x;
-        });
+        // sort ascending by x
+        std::sort(lightBlobs.begin(), lightBlobs.end(),
+                  [](const rm::LightBlob &lightBlob1, const rm::LightBlob &lightBlob2) {
+                      return lightBlob1.center.x < lightBlob2.center.x;
+                  });
 
         float yDifHistory = -1;
         int lastJ = -1;
+        bool rank = !(attention.x == -1.0 && attention.y == -1.0);
 
         for (int i = 0; i < lightBlobs.size() - 1; i++) {
             if (lightBlobs[i].camp == ownCamp) continue;
             for (int j = i + 1; j < lightBlobs.size(); j++) {
-                if (lightBlobs[j].camp == ownCamp) continue;
-                if (rm::LightBlobInterference(lightBlobs, i, j))continue;
+                if (lightBlobs[j].camp == ownCamp || rm::LightBlobOverlap(lightBlobs, i, j)) continue;
 
                 // Poor angle between two light blob
                 float angleDif = abs(lightBlobs[i].angle - lightBlobs[j].angle);
@@ -138,26 +127,27 @@ namespace rm {
                     ((lightBlobs[i].size.height + lightBlobs[j].size.height) / 2))
                     continue;
 
-                cv::Point2f centerArmour((lightBlobs[i].center.x + lightBlobs[j].center.x) / 2.0f,
-                                         (lightBlobs[i].center.y + lightBlobs[j].center.y) / 2.0f);
-
-                float yDiff = min(lightBlobs[j].size.height, lightBlobs[i].size.height);
-                if (lastJ == i) {
-                    if (yDifHistory < yDiff) {
-                        armours.pop_back();
-                    } else {
-                        if (filter) continue;
+                if (filter) {
+                    float yDiff = min(lightBlobs[j].size.height, lightBlobs[i].size.height);
+                    if (lastJ == i) {
+                        if (yDifHistory < yDiff) {
+                            armours.pop_back();
+                        } else {
+                            continue;
+                        }
                     }
+                    yDifHistory = yDiff;
+                    lastJ = j;
                 }
-                yDifHistory = yDiff;
-                lastJ = j;
 
-                armours.push_back(rm::Armour({lightBlobs[i], lightBlobs[j]}, rm::PointDistance(centerArmour, attention),
+                armours.push_back(rm::Armour({lightBlobs[i], lightBlobs[j]}, rank ? rm::PointDistance(
+                                                     {(lightBlobs[i].center.x + lightBlobs[j].center.x) / 2.0f,
+                                                      (lightBlobs[i].center.y + lightBlobs[j].center.y) / 2.0f}, attention) : 0,
                                              lightBlobs[i].camp));
             }
         }
 
-        if (armours.size() > 1) {
+        if (rank && armours.size() > 1) {
             std::sort(armours.begin(), armours.end(), [](const rm::Armour &armour1, const rm::Armour &armour2) {
                 return armour1.rank < armour2.rank;
             });
@@ -166,7 +156,7 @@ namespace rm {
 
     void
     SolveShootFactor(cv::Mat &translationVector, rm::ShootFactor &shootFactor, double g, double v0, double deltaHeight,
-                     cv::Point2f offset, rm::CompensateMode mode) {
+                     cv::Point2f offset, double angleOffset, rm::CompensateMode mode) {
         float yaw = (float) atan2(translationVector.ptr<double>(0)[0] - offset.x, translationVector.ptr<double>(0)[2]) *
                     180.0f / (float) CV_PI;
         float pitch = 0;
@@ -175,8 +165,8 @@ namespace rm {
         double d = (translationVector.ptr<double>(0)[2]) / 100.0;
 
         if (mode == rm::COMPENSATE_NONE) {
-            pitch = (float) atan2(translationVector.ptr<double>(0)[1] - offset.y, translationVector.ptr<double>(0)[2]) *
-                    180.0f / (float) CV_PI;
+            pitch = (-1) * ((float) atan2(translationVector.ptr<double>(0)[1] - offset.y,
+                                          translationVector.ptr<double>(0)[2]) * 180.0f / (float) CV_PI);
             airTime = d / v0;
         } else if (mode == rm::COMPENSATE_CLASSIC) {
             double normalAngle = atan2(deltaHeight / 100.0, d) * 180.0 / CV_PI;
@@ -185,7 +175,7 @@ namespace rm {
                     180.0 / CV_PI;
             double targetAngle = rm::ProjectileAngle(v0, g, d, deltaHeight / 100.0) * 180.0 / CV_PI;
 
-            pitch = (float) ((centerAngle - normalAngle) + targetAngle);
+            pitch = (float) ((centerAngle - normalAngle + angleOffset) + targetAngle);
             airTime = d / abs(v0 * cos(targetAngle));
         } else if (mode == rm::COMPENSATE_NI) {
             //TODO: Fix the bug of NI first!!!!
@@ -194,6 +184,11 @@ namespace rm {
         shootFactor.pitchAngle = pitch;
         shootFactor.yawAngle = yaw;
         shootFactor.estimateAirTime = airTime;
+    }
+
+    void SolveShootFactor(rm::ShootFactor &shootFactor, double d, double g, double v0, double h, cv::Point2f offset,
+                          double angleOffset, rm::CompensateMode mode) {
+
     }
 
     void SolveCameraPose(cv::Mat &rvecs, cv::Mat &tvecs, cv::Mat &output) {

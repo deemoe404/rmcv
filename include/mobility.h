e2bd82843b4e073a42ca67d0c8d5b6a4073cca97
EarthOL-088217 <90408077+EarthOL-088217@users.noreply.github.com>
2022-07-12 00:40:21 +0800

    Make modules in mobility doxygen-able

---
+++ include/mobility.h
@@ -0,0 +1,111 @@
+///
+/// \file      mobility.h
+/// \brief     Provides a series of functions for solving automatic control parameters.
+/// \version   v0.1.0
+/// \date      7/11/2022
+/// \author    EarthOL-088217
+/// \copyright Copyright (c) 2020-2022 and all right reserved.
+///
+
+#ifndef RMCV_MOBILITY_H
+#define RMCV_MOBILITY_H
+
+#include "core/core.h"
+
+namespace rm {
+    /// \brief Define witch method is used to calculate the compensation of gravity.
+    [[maybe_unused]] typedef enum CompensateMode {
+        COMPENSATE_NONE = 0,    ///< No compensation
+        COMPENSATE_CLASSIC = 1, ///< Use newtonian's theorem of mechanics
+        COMPENSATE_NI = 2       ///< Use newton iteration method
+    } CompensateMode;
+
+    /// \brief Rotate the vector around the x-axis.
+    /// \param y      y of the vector.
+    /// \param z      z of the vector.
+    /// \param thetaX angle to be rotate.
+    /// \param outY   y of the vector after rotation.
+    /// \param outZ   z of the vector after rotation.
+    void AxisRotateX(double y, double z, double thetaX, double &outY, double &outZ);
+
+    /// \brief Rotate the vector around the y-axis.
+    /// \param x      x of the vector.
+    /// \param z      z of the vector.
+    /// \param thetaY angle to be rotate.
+    /// \param outX   x of the vector after rotation.
+    /// \param outZ   z of the vector after rotation.
+    void AxisRotateY(double x, double z, double thetaY, double &outX, double &outZ);
+
+    /// \brief Rotate the vector around the z-axis.
+    /// \param x      x of the vector.
+    /// \param y      y of the vector.
+    /// \param thetaZ angle to be rotate.
+    /// \param outX   x of the vector after rotation.
+    /// \param outY   y of the vector after rotation.
+    void AxisRotateZ(double x, double y, double thetaZ, double &outX, double &outY);
+
+    /// \brief Solve height difference between barrel and target.
+    /// \param translationVector The translation vector of target.
+    /// \param motorAngle        The motor angle of gimbal, positive upwards. (RAD)
+    /// \param offset            Offset between camera and barrel.            (cm)
+    /// \param angleOffset       Angle offset between camera and barrel.      (RAD)
+    /// \return Height difference between barrel and target in cm, NAN if translationVector is not in cv::Mat format.
+    [[maybe_unused]] double
+    DeltaHeight(cv::InputArray translationVector, double motorAngle, const cv::Point2f &offset = {0, 0},
+                double angleOffset = 0);
+
+    /// \brief Solve the distance between camera and target.
+    /// \param translationVector The translation vector of target.
+    /// \return Distance in cm, NAN if translationVector is not in cv::Mat format.
+    [[maybe_unused]] double Distance(cv::InputArray translationVector);
+
+    /// \brief Solve initial angle required for a projectile motion.
+    ///
+    /// This function would be done by solving \f$ 0 = \frac{g \cdot d^{2}}{2 \cdot v_{0}^{2}} \cdot tan\theta^{2} + d
+    /// \cdot tan\theta + (\frac{g \cdot d^{2}}{2 \cdot v_{0}^{2}} - h) \f$ for \f$tan\theta\f$ using the binary first-order
+    /// root formula, witch can be deduced from Newtonian's theorem of mechanics.
+    ///
+    /// \param v0 Initial launch speed.    (m/s)
+    /// \param g  Acceleration of gravity. (m/s^2)
+    /// \param d  Horizontal distance.     (m)
+    /// \param h  Height difference.       (m)
+    /// \return Estimated launch angle in radians, NAN if equations have no real solutions.
+    double ProjectileAngle(double v0, double g, double d, double h);
+
+    /// \brief Solve camera pose in pitch, yaw, and roll relative to a target.
+    /// \param rotationVector    The rotation vector of target.
+    /// \param translationVector The translation vector of target.
+    /// \param pose              [OUT] Camera pose.
+    /// \return False if translationVector or rotationVector is not in cv::Mat format.
+    [[maybe_unused]] bool
+    SolveCameraPose(cv::InputArray rotationVector, cv::InputArray translationVector, cv::OutputArray pose);
+
+    /// \brief Solve gimbal error angle to target by given method.
+    /// \param translationVector Translation vector of target.
+    /// \param gimbalErrorAngle  [OUT] Estimation error angle. Format: [pitch, yaw].
+    /// \param g                 Acceleration of gravity.                (m/s^2)
+    /// \param v0                The initial speed of bullet.            (m/s)
+    /// \param h                 Height between barrel and target.       (m)
+    /// \param offset            Offset between camera and barrel.       (cm)
+    /// \param angleOffset       Angle offset between camera and barrel. (RAD)
+    /// \param mode              Method to be used to calculate the compensation of gravity.
+    /// \return Estimation air time, NAN if translationVector is not in cv::Mat format.
+    [[maybe_unused]] double
+    SolveGEA(cv::InputArray translationVector, cv::OutputArray gimbalErrorAngle, double g, double v0, double h,
+             const cv::Point2f &offset = {0, 0}, double angleOffset = 0, rm::CompensateMode mode = rm::COMPENSATE_NONE);
+
+    /// \brief Solve the rotation & translation vector using cv::solvePnP & cv::SOLVEPNP_IPPE_SQUARE.
+    /// \param imagePoints       Object points on image (Quantity must be four).
+    /// \param cameraMatrix      Camera matrix.
+    /// \param distortionFactor  Camera distortion factor.
+    /// \param exactSize         Exact size of the coordinate object (cm).
+    /// \param translationVector [OUT] Translation vector.
+    /// \param rotationVector    [OUT] Rotation vector.
+    /// \return False if imagePoints.size() is not 4, or there might be errors in Camera-parameters.
+    [[maybe_unused]] bool
+    SolvePNP(const std::vector<cv::Point2f> &imagePoints, cv::InputArray cameraMatrix, cv::InputArray distortionFactor,
+             const cv::Size2f &exactSize, cv::OutputArray translationVector, cv::OutputArray rotationVector,
+             const cv::Rect &ROI = {0, 0, 0, 0});
+}
+
+#endif //RMCV_MOBILITY_H
---
+++ src/mobility.cpp
@@ -0,0 +1,171 @@
+//
+// Created by yaione on 7/11/2022.
+//
+
+#include "include/mobility.h"
+
+namespace rm {
+    void AxisRotateX(double y, double z, double thetaX, double &outY, double &outZ) {
+        double y1 = y;
+        double z1 = z;
+        double rx = thetaX * CV_PI / 180;
+        outY = cos(rx) * y1 - sin(rx) * z1;
+        outZ = cos(rx) * z1 + sin(rx) * y1;
+    }
+
+    void AxisRotateY(double x, double z, double thetaY, double &outX, double &outZ) {
+        double x1 = x;
+        double z1 = z;
+        double ry = thetaY * CV_PI / 180;
+        outX = cos(ry) * x1 + sin(ry) * z1;
+        outZ = cos(ry) * z1 - sin(ry) * x1;
+    }
+
+    void AxisRotateZ(double x, double y, double thetaZ, double &outX, double &outY) {
+        double x1 = x;
+        double y1 = y;
+        double rz = thetaZ * CV_PI / 180;
+        outX = cos(rz) * x1 - sin(rz) * y1;
+        outY = sin(rz) * x1 + cos(rz) * y1;
+    }
+
+    double DeltaHeight(cv::InputArray translationVector, const double motorAngle, const cv::Point2f &offset,
+                       const double angleOffset) {
+        if (translationVector.kind() == cv::_InputArray::MAT) {
+            cv::Mat tvecs = translationVector.getMat();
+
+            double h = tvecs.ptr<double>(0)[1] - offset.y;
+            double d = tvecs.ptr<double>(0)[2];
+
+            double dPitch = -atan2(h, d) + (motorAngle - angleOffset);
+
+            return d * tan(dPitch);
+        }
+        return NAN;
+    }
+
+    [[maybe_unused]] double Distance(cv::InputArray translationVector) {
+        if (translationVector.kind() == cv::_InputArray::MAT) {
+            cv::Mat tvecs = translationVector.getMat();
+            return sqrt(pow(tvecs.at<double>(0), 2) + pow(tvecs.at<double>(1), 2) + pow(tvecs.at<double>(2), 2));
+        }
+        return NAN;
+    }
+
+    double ProjectileAngle(const double v0, const double g, const double d, const double h) {
+        double a = (g * pow(d, 2.0)) / (2.0 * pow(v0, 2.0));
+        double b = d;
+        double c = a - h;
+
+        double delta = pow(b, 2.0) - (4 * a * c);
+        if (delta > 0) {
+            double x1 = atan(((-1 * b) + sqrt(delta)) / (2 * a));
+            double x2 = atan(((-1 * b) - sqrt(delta)) / (2 * a));
+            return abs(x1) < abs(x2) ? x1 : x2;
+        } else if (delta == 0) {
+            return atan((-1) * (b / 2 * a));
+        }
+
+        return NAN;
+    }
+
+    [[maybe_unused]] bool
+    SolveCameraPose(cv::InputArray rotationVector, cv::InputArray translationVector, cv::OutputArray pose) {
+        if (translationVector.kind() == cv::_InputArray::MAT && rotationVector.kind() == cv::_InputArray::MAT) {
+            pose.create({3, 1}, cv::_OutputArray::MAT);
+            cv::Mat output = pose.getMat(), rvecs = rotationVector.getMat(), tvecs = translationVector.getMat();
+
+            cv::Mat rotT = cv::Mat::eye(3, 3, CV_64F);
+            cv::Rodrigues(tvecs, rotT);
+
+            double rm[9];
+            cv::Mat rotM(3, 3, CV_64FC1, rm);
+            cv::Rodrigues(rvecs, rotM);
+            double r11 = rotM.ptr<double>(0)[0];
+            double r21 = rotM.ptr<double>(1)[0];
+            double r31 = rotM.ptr<double>(2)[0];
+            double r32 = rotM.ptr<double>(2)[1];
+            double r33 = rotM.ptr<double>(2)[2];
+
+            double thetaZ = atan2(r21, r11) / CV_PI * 180;
+            double thetaY = atan2(-1 * r31, sqrt(r32 * r32 + r33 * r33)) / CV_PI * 180;
+            double thetaX = atan2(r32, r33) / CV_PI * 180;
+
+            double tx = tvecs.ptr<double>(0)[0];
+            double ty = tvecs.ptr<double>(0)[1];
+            double tz = tvecs.ptr<double>(0)[2];
+
+            double x = tx, y = ty, z = tz;
+
+            AxisRotateZ(x, y, -1 * thetaZ, x, y);
+            AxisRotateY(x, z, -1 * thetaY, x, z);
+            AxisRotateX(y, z, -1 * thetaX, y, z);
+
+            output.ptr<float>(0)[0] = (float) thetaX * -1;
+            output.ptr<float>(0)[1] = (float) thetaY * -1;
+            output.ptr<float>(0)[2] = (float) thetaZ * -1;
+
+            return true;
+        }
+        return false;
+    }
+
+    [[maybe_unused]] double
+    SolveGEA(cv::InputArray translationVector, cv::OutputArray gimbalErrorAngle, const double g, const double v0,
+             const double h, const cv::Point2f &offset, const double angleOffset, const rm::CompensateMode mode) {
+        if (translationVector.kind() == cv::_InputArray::MAT) {
+            cv::Mat tvecs = translationVector.getMat();
+            double p = 0, t = 0, d = tvecs.ptr<double>(0)[2] / 100.0, y =
+                    atan2(tvecs.ptr<double>(0)[0] - offset.x, tvecs.ptr<double>(0)[2]) * 180.0 / CV_PI;
+
+            switch (mode) {
+                case rm::COMPENSATE_NONE:
+                    p = -(atan2(tvecs.ptr<double>(0)[1] - offset.y, tvecs.ptr<double>(0)[2]) * 180.0 / CV_PI);
+                    t = d / v0;
+                    break;
+                case COMPENSATE_CLASSIC:
+                    double normalAngle, centerAngle, targetAngle;
+                    normalAngle = atan2(h / 100.0, d) * 180.0 / CV_PI;
+                    centerAngle = -atan2(tvecs.ptr<double>(0)[1] - offset.y, tvecs.ptr<double>(0)[2]) * 180.0 / CV_PI;
+                    targetAngle = rm::ProjectileAngle(v0, g, d, h / 100.0) * 180.0 / CV_PI;
+
+                    p = (centerAngle - normalAngle + angleOffset * 180.0 / CV_PI) + targetAngle;
+                    t = d / abs(v0 * cos(targetAngle));
+                    break;
+                case COMPENSATE_NI:
+                    return NAN; //TODO: Fix the bug of NI first!!!!
+            }
+
+            gimbalErrorAngle.create({2, 1}, cv::_OutputArray::MAT);
+            cv::Mat gea = gimbalErrorAngle.getMat();
+            gea.ptr<double>(0)[0] = p;
+            gea.ptr<double>(0)[1] = y;
+            return t;
+        }
+
+        return NAN;
+    }
+
+    [[maybe_unused]] bool
+    SolvePNP(const std::vector<cv::Point2f> &imagePoints, cv::InputArray cameraMatrix, cv::InputArray distortionFactor,
+             const cv::Size2f &exactSize, cv::OutputArray translationVector, cv::OutputArray rotationVector,
+             const cv::Rect &ROI) {
+        if (imagePoints.size() == 4) {
+            cv::Point2f offset((float) ROI.x, (float) ROI.y);
+
+            std::vector<cv::Point3f> exactPoint{cv::Point3f(-exactSize.width / 2.0f, exactSize.height / 2.0f, 0),
+                                                cv::Point3f(exactSize.width / 2.0f, exactSize.height / 2.0f, 0),
+                                                cv::Point3f(exactSize.width / 2.0f, -exactSize.height / 2.0f, 0),
+                                                cv::Point3f(-exactSize.width / 2.0f, -exactSize.height / 2.0f, 0)};
+
+            // TODO: sort points before doing this!!!
+            std::vector<cv::Point2f> coordinate{imagePoints[1] + offset, imagePoints[2] + offset,
+                                                imagePoints[3] + offset, imagePoints[0] + offset};
+
+            return cv::solvePnP(exactPoint, coordinate, cameraMatrix, distortionFactor, rotationVector,
+                                translationVector, false, cv::SOLVEPNP_IPPE_SQUARE);
+        }
+
+        return false;
+    }
+}

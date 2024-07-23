6c7bec55f2b6d6d882b5f316e485e4c23dddb9e0
deemoe <me@dee.moe>
2024-07-23 17:22:54 +0800

    Merged core and utils

---
+++ include/core.h
@@ -0,0 +1,220 @@
+//
+// Created by yaione on 2/26/2022.
+//
+
+#ifndef RMCV_CORE_H
+#define RMCV_CORE_H
+
+#include <cmath>
+#include <vector>
+#include <string>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <filesystem>
+#include <cstring>
+#include "opencv2/opencv.hpp"
+
+#include <string>
+#include <algorithm>
+#include <filesystem>
+#include <random>
+#include <opencv2/ml.hpp>
+
+namespace rm
+{
+    enum color_camp
+    {
+        CAMP_RED = 0, CAMP_BLUE = 1, CAMP_GUIDELIGHT = 2, CAMP_NEUTRAL = -1
+    };
+
+    enum AimMode
+    {
+        AIM_COMBAT = 0, AIM_BUFF = 1, AIM_SNIPE = 2
+    };
+
+    enum RectType
+    {
+        RECT_TALL = 0, RECT_SIDE = 1
+    };
+
+    enum FileType
+    {
+        FILETYPE_REGULAR_FILE = 0,
+        FILETYPE_DIRECTORY = 1,
+        FILETYPE_SYMBOLIC_LINK = 2,
+        FILETYPE_SOCKET = 3,
+        FILETYPE_UNKNOWN = -1
+    };
+
+    template <typename T>
+    struct range
+    {
+        T lower_bound;
+        T upper_bound;
+
+        range(T lower, T upper) : lower_bound(lower), upper_bound(upper)
+        {
+        }
+
+        [[nodiscard]] bool contains(T value) const
+        {
+            return value >= lower_bound && value <= upper_bound;
+        }
+    };
+
+    struct parameters
+    {
+        int extraction_lower_bound = 80;
+
+        float lightblob_tilt_max = 70.0;
+        range<float> lightblob_ratio = {1.5, 80.0};
+        range<double> lightblob_area = {10, 99999};
+
+        float armour_angle_difference_max = 12;
+        float armour_shear_max = 22;
+        float armour_lenght_ratio_max = 12;
+
+        parameters() = default;
+
+        explicit parameters(const std::string& path)
+        {
+        }
+    };
+
+    typedef std::vector<cv::Point> contour;
+
+    class lightblob
+    {
+    public:
+        float angle = 0; /// Rotation angle of the light blob (vertical when the angle is 90)
+        rm::color_camp camp = rm::CAMP_NEUTRAL; /// Camp this light blob belongs to
+        cv::Point2f center; /// Mass center of the light blob
+        cv::Point2f vertices[4]; /// Four vertices around the light blob
+        cv::Size2f size; /// Width and height of the light blob
+
+        lightblob() = default;
+
+        explicit lightblob(cv::RotatedRect box, rm::color_camp camp = rm::CAMP_NEUTRAL);
+    };
+
+    class armour
+    {
+    public:
+        float rank = 0; /// A value help in sorting multiple armours
+        rm::color_camp camp = rm::CAMP_NEUTRAL; /// Camp this armour belongs to
+        cv::Point2f icon[4]; /// Vertices of icon area
+        cv::Point2f vertices[4]; /// Vertices of armour (square with light blob as side length for better PNP result)
+
+        armour() = default;
+
+        explicit armour(std::vector<rm::lightblob> lightBlobs, float rank = 0, rm::color_camp camp = rm::CAMP_NEUTRAL);
+    };
+
+    class Package
+    {
+    public:
+        rm::color_camp camp = rm::CAMP_NEUTRAL; /// Self camp
+        rm::AimMode mode = rm::AIM_COMBAT; /// Aim mode
+        unsigned char speed = 0; /// Bullet speed
+        float pitch = 0; /// Pitch angle
+        cv::Mat frame;
+        cv::Mat binary;
+        std::vector<rm::armour> armours;
+
+        Package(rm::color_camp camp, rm::AimMode mode, unsigned char speed, float pitch, const cv::Mat& inputFrame,
+                const cv::Mat& inputBinary);
+
+        explicit Package(const std::shared_ptr<rm::Package>& input);
+    };
+}
+
+namespace rm::utils
+{
+    std::vector<std::filesystem::path> list_directory_recursive(const std::filesystem::path& directory,
+                                                                const std::vector<std::string>& extension_whitelist =
+                                                                    {});
+
+    std::vector<cv::Mat> read_image_recursive(const std::filesystem::path& directory);
+
+    FileType GetFileType(const char* filename);
+
+    bool ListFiles(const char* path, std::vector<std::string>& filenames);
+
+    cv::Rect
+    GetROI(cv::Point2f* imagePoints, int pointsCount, float scaleFactor = 1.0f, const cv::Size& frameSize = {-1, -1},
+           const cv::Rect& previous = {0, 0, 0, 0});
+
+    cv::Rect GetROI(cv::Point2f* imagePoints, int pointsCount, const cv::Size2f& scaleFactor = {1, 1},
+                    const cv::Size& frameSize = {-1, -1}, const cv::Rect& previous = {0, 0, 0, 0});
+
+    /// Reorder vertices of a rectangle to match the point order in left down, left up, right up, right down.
+    /// \param input The original rectangle.
+    /// \param output Reordered vertices.
+    /// \param type Rectangle type.
+    void reorder_vertices(cv::RotatedRect& input, cv::Point2f* output, RectType type);
+
+    /// Calibrate given point sets in to specified aspect ratio.
+    /// \param input Origin point sets.
+    /// \param output Points sets in specified aspect ratio.
+    /// \param outRatio Aspect ratio.
+    void CalcPerspective(cv::Point2f input[4], cv::Point2f output[4], float outRatio = 1.0f);
+
+    /// Use newton's iteration to approach the approx solve of function.
+    /// \param fd The f(x)/f'(x) function of goal function.
+    /// \param x0 x to start the iteration.
+    /// \param error Maximum error before iteration stops.
+    /// \param cycle Maximum cycle the iteration could run.
+    /// \return The solve.
+    double NewtonIteration(double (*fd)(double), double x0 = 0, double error = 0.0001, int cycle = 1024);
+
+    /// Use newton's iteration to approach the approx solve of function.
+    /// \param fd The f(x)/f'(x) function of goal function.
+    /// \param literals The literals that all f(x) needed.
+    /// \param x0 x to start the iteration.
+    /// \param error Maximum error before iteration stops.
+    /// \param cycle Maximum cycle the iteration could run.
+    /// \return The solve.
+    double
+    NewtonIteration(double (*fd)(double, std::vector<double>), const std::vector<double>& literals, double x0 = 0,
+                    double error = 0.0001, int cycle = 1024);
+
+    /// The f(x)/f'(x) function of projectile motion.
+    /// \param theta The angle of the initial shot of the oblique throwing motion, in radians.
+    /// \param literals The independent variables, in the order of g, d, h, v0.
+    /// \return f(x)/f'(x)
+    double ProjectileMotionFD(double theta, std::vector<double> literals);
+
+
+    /// Return the distance between two given points.
+    /// \param pt1 First point.
+    /// \param pt2 Second point.
+    /// \return Distance.
+    float PointDistance(const cv::Point2f& pt1, const cv::Point2f& pt2);
+
+    /// Return the distance between two given points.
+    /// \param pt1 First point.
+    /// \param pt2 Second point.
+    /// \return Distance.
+    float PointDistance(const cv::Point2i& pt1, const cv::Point2i& pt2);
+
+    /// Return the midpoint between two given points.
+    /// \param pt1 First point.
+    /// \param pt2 Second point.
+    /// \return Midpoint.
+    cv::Point2f LineCenter(const cv::Point2f& pt1, const cv::Point2f& pt2);
+
+    /// Expand the cord by the given length while center of the cord remain still.
+    /// \param pt1 First point of the cord.
+    /// \param pt2 Second point of the cord.
+    /// \param deltaLen The length to be expanded.
+    /// \param dst1 The first point after expanding.
+    /// \param dst2 The second point after expanding.
+    void
+    ExtendCord(const cv::Point2f& pt1, const cv::Point2f& pt2, float deltaLen, cv::Point2f& dst1, cv::Point2f& dst2);
+
+    std::string PathCombine(const std::string& path1, const std::string& path2);
+
+    std::string int2str(int number);
+}
+
+#endif //RMCV_CORE_H
--- src/core.cpp
+++ src/core.cpp
@@ -2,32 +2,32 @@
 // Created by yaione on 2/26/2022.
 //
 
-#include "include/core/core.h"
+#include "core.h"
 
 namespace rm
 {
-    LightBlob::LightBlob(cv::RotatedRect box, const CampType camp) :
+    lightblob::lightblob(cv::RotatedRect box, const color_camp camp) :
         angle(
             box.angle > 90
                 ? box.angle - 90
                 : box.angle + 90), camp(camp),
         center(box.center)
     {
-        reorder_vertices(box, this->vertices, RECT_TALL);
+        utils::reorder_vertices(box, this->vertices, RECT_TALL);
 
         this->size = {std::min(box.size.height, box.size.width), std::max(box.size.height, box.size.width)};
     }
 
-    Armour::Armour(std::vector<rm::LightBlob> lightBlobs, float rank, rm::CampType camp) : camp(camp), rank(rank)
+    armour::armour(std::vector<rm::lightblob> lightBlobs, float rank, rm::color_camp camp) : camp(camp), rank(rank)
     {
         if (lightBlobs.size() != 2)
         {
-            throw std::runtime_error("Armour must be initialized with 2 rm::LightBlob (s).");
+            throw std::runtime_error("armour must be initialized with 2 rm::lightblob (s).");
         }
 
         // sort light blobs left to right
         std::sort(lightBlobs.begin(), lightBlobs.end(),
-                  [](const rm::LightBlob& lightBar1, const rm::LightBlob& lightBar2)
+                  [](const rm::lightblob& lightBar1, const rm::lightblob& lightBar2)
                   {
                       return lightBar1.center.x < lightBar2.center.x;
                   });
@@ -39,17 +39,17 @@ namespace rm
             vertices[i++] = lightBar.vertices[j--];
         }
 
-        float distanceL = rm::PointDistance(vertices[0], vertices[1]);
-        float distanceR = rm::PointDistance(vertices[2], vertices[3]);
+        float distanceL = rm::utils::PointDistance(vertices[0], vertices[1]);
+        float distanceR = rm::utils::PointDistance(vertices[2], vertices[3]);
         float offsetL = round((distanceL / 0.50f - distanceL) / 2);
         float offsetR = round((distanceR / 0.50f - distanceR) / 2);
-        rm::ExtendCord(vertices[0], vertices[1], offsetL, icon[0], icon[1]);
-        rm::ExtendCord(vertices[3], vertices[2], offsetR, icon[3], icon[2]);
+        rm::utils::ExtendCord(vertices[0], vertices[1], offsetL, icon[0], icon[1]);
+        rm::utils::ExtendCord(vertices[3], vertices[2], offsetR, icon[3], icon[2]);
 
-        rm::CalcPerspective(vertices, vertices);
+        rm::utils::CalcPerspective(vertices, vertices);
     }
 
-    Package::Package(rm::CampType camp, rm::AimMode mode, unsigned char speed, float pitch, const cv::Mat& inputFrame,
+    Package::Package(rm::color_camp camp, rm::AimMode mode, unsigned char speed, float pitch, const cv::Mat& inputFrame,
                      const cv::Mat& inputBinary) : camp(camp), mode(mode), speed(speed), pitch(pitch)
     {
         inputFrame.copyTo(this->frame);
@@ -61,6 +61,355 @@ namespace rm
     {
         input->frame.copyTo(this->frame);
         input->binary.copyTo(this->binary);
-        this->armours = std::vector<rm::Armour>(input->armours);
+        this->armours = std::vector<rm::armour>(input->armours);
+    }
+}
+
+namespace rm::utils
+{
+    std::vector<std::filesystem::path> list_directory_recursive(const std::filesystem::path& directory,
+                                                                const std::vector<std::string>& extension_whitelist)
+    {
+        std::vector<std::filesystem::path> files;
+        for (const auto& p : std::filesystem::recursive_directory_iterator(directory))
+        {
+            if (std::filesystem::is_directory(p)) continue;
+            if (!extension_whitelist.empty())
+            {
+                auto extension = p.path().extension().string();
+                auto head = extension_whitelist.begin(),
+                     tail = extension_whitelist.end();
+                if (auto iterator = find(head, tail, extension); iterator != tail)
+                    files.push_back(p.path());
+            }
+            else files.push_back(p.path());
+        }
+        return files;
+    }
+
+    std::vector<cv::Mat> read_image_recursive(const std::filesystem::path& directory)
+    {
+        std::vector<cv::Mat> images;
+        const auto files = list_directory_recursive(directory, {".jpg"});
+        for_each(files.begin(), files.end(), [&images](const std::filesystem::path& p)
+        {
+            cv::Mat image = cv::imread(p.string());
+            resize(image, image, {20, 20}, 0, 0, cv::INTER_LINEAR);
+            image = image.reshape(1, 1);
+            image.convertTo(image, CV_32FC1);
+            images.push_back(image);
+        });
+        return images;
+    }
+
+    rm::FileType GetFileType(const char* filename)
+    {
+        struct stat buffer{};
+        if (stat(filename, &buffer) == 0)
+        {
+            if (S_ISREG(buffer.st_mode))
+                return rm::FILETYPE_REGULAR_FILE;
+
+            if (S_ISDIR(buffer.st_mode))
+                return rm::FILETYPE_DIRECTORY;
+
+            if (S_ISLNK(buffer.st_mode))
+                return rm::FILETYPE_SYMBOLIC_LINK;
+
+            if (S_ISSOCK(buffer.st_mode))
+                return rm::FILETYPE_SOCKET;
+        }
+
+        return rm::FILETYPE_UNKNOWN;
+    }
+
+    bool ListFiles(const char* path, std::vector<std::string>& filenames)
+    {
+        // NOLINT(misc-no-recursion)
+        DIR* pDir;
+        struct dirent* ptr;
+        if (!(pDir = opendir(path)))
+        {
+            return false;
+        }
+
+        std::filesystem::path fullPath;
+        while ((ptr = readdir(pDir)) != nullptr)
+        {
+            fullPath = path;
+            if (strcmp(ptr->d_name, ".") != 0 && strcmp(ptr->d_name, "..") != 0)
+            {
+                fullPath /= ptr->d_name;
+                if (GetFileType(fullPath.string().c_str()) == rm::FILETYPE_DIRECTORY)
+                {
+                    ListFiles(fullPath.string().c_str(), filenames);
+                }
+                else
+                {
+                    filenames.push_back(fullPath.string());
+                }
+            }
+        }
+
+        closedir(pDir);
+        return true;
+    }
+
+    cv::Rect GetROI(cv::Point2f* imagePoints, int pointsCount, float scaleFactor, const cv::Size& frameSize,
+                    const cv::Rect& previous)
+    {
+        return GetROI(imagePoints, pointsCount, {scaleFactor, scaleFactor}, frameSize, previous);
+    }
+
+    cv::Rect GetROI(cv::Point2f* imagePoints, int pointsCount, const cv::Size2f& scaleFactor, const cv::Size& frameSize,
+                    const cv::Rect& previous)
+    {
+        cv::Rect boundingRect = cv::boundingRect(std::vector<cv::Point2f>(imagePoints, imagePoints + pointsCount));
+        boundingRect.x += previous.x;
+        boundingRect.y += previous.y;
+        if (scaleFactor.width != 1.0f || scaleFactor.height != 1.0f)
+        {
+            cv::Size scale((int)((double)boundingRect.width * scaleFactor.width / 2.0),
+                           (int)((double)boundingRect.height * scaleFactor.height / 2.0));
+
+            boundingRect.x -= scale.width;
+            boundingRect.y -= scale.height;
+            boundingRect.width += scale.width * 2;
+            boundingRect.height += scale.width * 2;
+        }
+        if (boundingRect.x < 0)
+        {
+            boundingRect.x = 0;
+        }
+        if (boundingRect.y < 0)
+        {
+            boundingRect.y = 0;
+        }
+        if (boundingRect.x + boundingRect.width >= frameSize.width)
+        {
+            boundingRect.width = frameSize.width - boundingRect.x - 1;
+        }
+        if (boundingRect.y + boundingRect.height >= frameSize.height)
+        {
+            boundingRect.height = frameSize.height - boundingRect.y - 1;
+        }
+
+        if (boundingRect.width < 0 || boundingRect.height < 0)
+        {
+            return {0, 0, 0, 0};
+        }
+
+        return boundingRect;
+    }
+
+    void reorder_vertices(cv::RotatedRect& input, cv::Point2f* output, RectType type = RECT_TALL)
+    {
+        cv::Point2f temp[4];
+        input.points(temp);
+
+        // sort vertices ascending by y
+        std::sort(temp, temp + 4, [](const cv::Point2f& point1, const cv::Point2f& point2)
+        {
+            return point1.y < point2.y;
+        });
+
+        bool swap_up = temp[0].x < temp[1].x, swap_down = temp[2].x < temp[3].x;
+        output[0] = swap_down ? temp[2] : temp[3]; // left down
+        output[1] = swap_up ? temp[0] : temp[1]; // left up
+        output[2] = swap_up ? temp[1] : temp[0]; // right up
+        output[3] = swap_down ? temp[3] : temp[2]; // right down
+
+        // TODO: Add side support & handle some edge case.
+    }
+
+    double NewtonIteration(double (*fd)(double), double x0, double error, int cycle)
+    {
+        double a = x0;
+        double x = x0 - fd(x0);
+
+        while (fabs(x - a) > error && cycle--)
+        {
+            a = x;
+            x = x - fd(x);
+            if (a == x)
+            {
+                return x;
+            }
+        }
+        return x;
+    }
+
+    double NewtonIteration(double (*fd)(double, std::vector<double>), const std::vector<double>& literals, double x0,
+                           double error, int cycle)
+    {
+        double a = x0;
+        double x = x0 - fd(x0, literals);
+
+        while (fabs(x - a) > error && cycle--)
+        {
+            a = x;
+            x = x - fd(x, literals);
+            if (a == x)
+            {
+                return x;
+            }
+        }
+        return x;
+    }
+
+    double ProjectileMotionFD(double theta, std::vector<double> literals)
+    {
+        if (literals.size() == 4)
+        {
+            double g = literals[0], d = literals[1], h = literals[2], v0 = literals[3];
+            int towards = (int)theta ? (int)theta / abs((int)theta) : 1;
+
+            double fx = h - d * tan(theta) + towards * (pow(d, 2.0) * g / pow(cos(theta) * v0, 2.0) * 2.0);
+            double dx = towards * (pow(d, 2.0) * g * sin(theta)) / ((pow(v0, 2.0) * pow(cos(theta), 3.0))) -
+                d * (pow(tan(theta), 2.0) + 1);
+
+            return (1 * fx / dx);
+        }
+        else
+        {
+            return -1;
+        }
+    }
+
+
+    float PointDistance(const cv::Point2f& pt1, const cv::Point2f& pt2)
+    {
+        return (float)sqrt(pow(pt1.x - pt2.x, 2) + pow(pt1.y - pt2.y, 2));
+    }
+
+    float PointDistance(const cv::Point2i& pt1, const cv::Point2i& pt2)
+    {
+        return (float)sqrt(pow(pt1.x - pt2.x, 2) + pow(pt1.y - pt2.y, 2));
+    }
+
+    void
+    ExtendCord(const cv::Point2f& pt1, const cv::Point2f& pt2, float deltaLen, cv::Point2f& dst1, cv::Point2f& dst2)
+    {
+        // Special case
+        if (pt1.x == pt2.x)
+        {
+            dst1.x = pt1.x;
+            dst2.x = pt1.x;
+
+            if (pt1.y > pt2.y)
+            {
+                dst1.y = pt1.y + deltaLen;
+                dst2.y = pt2.y - deltaLen;
+            }
+            else
+            {
+                dst1.y = pt1.y - deltaLen;
+                dst2.y = pt2.y + deltaLen;
+            }
+        }
+        else if (pt1.y == pt2.y)
+        {
+            dst1.y = pt1.y;
+            dst2.y = pt1.y;
+
+            if (pt1.x > pt2.x)
+            {
+                dst1.x = pt1.x + deltaLen;
+                dst2.x = pt2.x - deltaLen;
+            }
+            else
+            {
+                dst1.x = pt1.x - deltaLen;
+                dst2.x = pt2.x + deltaLen;
+            }
+            // Common case
+        }
+        else
+        {
+            float k = (float)(pt1.y - pt2.y) / (float)(pt1.x - pt2.x);
+            float theta = atan2((float)abs(pt1.y - pt2.y), (float)abs(pt1.x - pt2.x));
+            float zoomY = sin(theta) * deltaLen;
+            float zoomX = cos(theta) * deltaLen;
+
+            // Left tilt
+            if (k > 0)
+            {
+                if (pt1.x > pt2.x)
+                {
+                    dst1.x = pt1.x + zoomX;
+                    dst1.y = pt1.y + zoomY;
+
+                    dst2.x = pt2.x - zoomX;
+                    dst2.y = pt2.y - zoomY;
+                }
+                else
+                {
+                    dst1.x = pt1.x - zoomX;
+                    dst1.y = pt1.y - zoomY;
+
+                    dst2.x = pt2.x + zoomX;
+                    dst2.y = pt2.y + zoomY;
+                }
+                // Right tilt
+            }
+            else
+            {
+                if (pt1.x < pt2.x)
+                {
+                    dst1.x = pt1.x - zoomX;
+                    dst1.y = pt1.y + zoomY;
+
+                    dst2.x = pt2.x + zoomX;
+                    dst2.y = pt2.y - zoomY;
+                }
+                else
+                {
+                    dst1.x = pt1.x + zoomX;
+                    dst1.y = pt1.y - zoomY;
+
+                    dst2.x = pt2.x - zoomX;
+                    dst2.y = pt2.y + zoomY;
+                }
+            }
+        }
+    }
+
+    std::string PathCombine(const std::string& path1, const std::string& path2)
+    {
+        std::filesystem::path fullPath = path1;
+        fullPath /= path2;
+        return fullPath.string();
+    }
+
+    std::string int2str(int number)
+    {
+        char str[64] = {0};
+        std::snprintf(str, 64, "%d", number);
+        std::string res(str);
+        return res;
+    }
+
+    void CalcPerspective(cv::Point2f input[4], cv::Point2f output[4], float outRatio)
+    {
+        float leftHeight = PointDistance(input[0], input[1]);
+        float rightHeight = PointDistance(input[2], input[3]);
+        float maxHeight = fmax(leftHeight, rightHeight);
+
+        cv::Size2f size((maxHeight * outRatio), maxHeight);
+        cv::Point2f center = LineCenter(LineCenter(input[0], input[1]), LineCenter(input[2], input[3]));
+
+        output[0].x = center.x - size.width / 2;
+        output[0].y = center.y - size.height / 2;
+        output[1].x = center.x - size.width / 2;
+        output[1].y = center.y + size.height / 2;
+        output[2].x = center.x + size.width / 2;
+        output[2].y = center.y + size.height / 2;
+        output[3].x = center.x + size.width / 2;
+        output[3].y = center.y - size.height / 2;
+    }
+
+    cv::Point2f LineCenter(const cv::Point2f& pt1, const cv::Point2f& pt2)
+    {
+        return {pt1.x / 2 + pt2.x / 2, pt1.y / 2 + pt2.y / 2};
     }
 }

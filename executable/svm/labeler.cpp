181e58a299051dc5bcd03bdfe00464f622b24043
deemoe <me@dee.moe>
2024-07-27 14:51:04 +0800

    Minor changes

--- executable/svm/labeler.cpp
+++ executable/svm/labeler.cpp
@@ -2,99 +2,132 @@
 // Created by Sam Cheung on 7/20/24.
 //
 
-#include "include/rmcv.h"
+#include "rmcv.h"
 
 class video_controller
 {
-    int index = 0;
+    int frame_index = 0;
+    rm::range<int> frame_range = {0, 0};
     cv::VideoCapture capture;
-    rm::range<int> frame_range;
 
 public:
     cv::Mat frame;
 
-    explicit video_controller(const std::string& VideoPath)
-        : capture(VideoPath),
-          frame_range(0, static_cast<int>(capture.get(cv::CAP_PROP_FRAME_COUNT)) - 1)
+    explicit video_controller(const std::string& video_path)
     {
+        capture.open(video_path);
+        frame_range = {0, static_cast<int>(capture.get(cv::CAP_PROP_FRAME_COUNT)) - 1};
         update(0);
     }
 
-    void update(const int offset)
+    bool update(const int offset)
     {
-        if (!frame_range.contains(index + offset))
-            index = offset > 0 ? frame_range.lower_bound : frame_range.upper_bound;
+        bool rested = false;
+        if (!frame_range.contains(frame_index + offset))
+        {
+            frame_index = offset > 0 ? frame_range.lower_bound : frame_range.upper_bound;
+            rested = true;
+        }
 
-        index = index + offset;
-        capture.set(cv::CAP_PROP_POS_FRAMES, index);
+        frame_index = frame_index + offset;
+        capture.set(cv::CAP_PROP_POS_FRAMES, frame_index);
         capture >> frame;
+
+        return !rested;
+    }
+
+    void reset()
+    {
+        frame_index = 0;
     }
 };
 
+///
+/// @param original_frame 
+/// @param parameters 
+/// @param target_camp 
+/// @return 
+auto reconizer(const cv::Mat& original_frame, const rm::parameters& parameters, const rm::color_camp target_camp)
+    -> std::tuple<cv::Mat, std::vector<rm::lightblob>, std::vector<rm::contour>, std::vector<rm::armour>>
+{
+    cv::Mat binary;
+    std::vector<rm::contour> contours;
+
+    extract_color(original_frame, binary, target_camp, parameters.extraction_lower_bound);
+    findContours(binary, contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_NONE);
+
+    std::vector<rm::lightblob> lightblobs_positive;
+    std::vector<rm::contour> lightblobs_negative;
+    filter_lightblobs(contours, lightblobs_positive, lightblobs_negative, parameters.lightblob_tilt_max,
+                      parameters.lightblob_ratio, parameters.lightblob_area, rm::CAMP_RED);
+
+    std::vector<rm::armour> armours;
+    filter_armours(lightblobs_positive, armours, parameters.armour_angle_difference_max,
+                   parameters.armour_shear_max, parameters.armour_lenght_ratio_max, rm::CAMP_RED);
+
+    return {binary, lightblobs_positive, lightblobs_negative, armours};
+}
+
 int main()
 {
-    video_controller video("./videos/output.avi");
+    video_controller video("./videos/output3.avi");
 
     rm::parameters red;
-    red.lower_bound = {32, 32, 0};
+    red.extraction_lower_bound = 80;
+    red.lightblob_tilt_max = 70;
+    red.lightblob_ratio = {1.5, 80};
+    red.lightblob_area = {10, 99999};
+    red.armour_angle_difference_max = 999;
+    red.armour_shear_max = 999;
+    red.armour_lenght_ratio_max = 0.01;
+
+    // read svm.xml
+    const cv::Ptr<cv::ml::SVM> svm = cv::ml::SVM::load("svm.xml");
 
     bool exit = false, playing = false;
     while (!exit)
     {
-        cv::Mat binary;
-        std::vector<rm::Contour> contours;
-        std::vector<rm::LightBlob> lightBlobs;
-        std::vector<rm::Armour> armours;
+        auto [binary, lightblobs_positive, lightblobs_negative, armours] =
+            reconizer(video.frame, red, rm::CAMP_RED);
 
-        inRange(video.frame, red.lower_bound, cv::Scalar(255, 255, 255), binary);
-        morphologyEx(binary, binary, cv::MORPH_OPEN, cv::getStructuringElement(cv::MORPH_RECT, {3, 3}));
-
-        // ExtractColor(video.frame, binary, rm::CAMP_RED, 80, false, {5, 5});
-        findContours(binary, contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_NONE);
-        FindLightBlobs(contours, lightBlobs, 2, 12, 25, 80, 1500, video.frame, false);
-        FindArmour(lightBlobs, armours, 10, 24, 0.15, 0.45, 0.65, rm::CAMP_RED);
-
-        std::vector<cv::Mat> num;
+        std::vector<cv::Mat> icon_images;
         for (auto armour : armours)
         {
             cv::Mat tmp;
-            rm::CalcRatio(video.frame, tmp, armour.icon, {20, 20});
-            num.push_back(tmp);
+            rm::CalcRatio(video.frame, tmp, armour.icon, {80, 80});
+            icon_images.push_back(tmp);
         }
 
         int i = 0;
-        for (const auto& n : num)
+        cv::Mat debug_icon_images(80, 80 * static_cast<int>(icon_images.size()), CV_8UC3);
+        for (const auto& n : icon_images)
         {
-            imshow(std::to_string(i++), n);
-            auto now = cv::getTickCount();
-            imwrite(std::to_string(now) + ".png", n);
+            n.copyTo(debug_icon_images(cv::Rect(80 * i++, 0, 80, 80)));
         }
 
         cv::Mat debug;
         video.frame.copyTo(debug);
         rm::debug::DrawArmours(armours, debug, -1);
-        rm::debug::DrawLightBlobs(lightBlobs, debug, -1);
-        drawContours(debug, contours, -1, cv::Scalar(0, 255, 0), 1);
+        rm::debug::draw_lightblobs(lightblobs_positive, lightblobs_negative, debug, -1);
 
-        imshow("Frame", debug);
-        imshow("Binary", binary);
+        cv::Mat combined = cv::Mat::zeros(debug.rows + 80, debug.cols + binary.cols, debug.type());
+        debug.copyTo(combined(cv::Rect(0, 0, debug.cols, debug.rows)));
+        cvtColor(binary, binary, cv::COLOR_GRAY2BGR);
+        binary.copyTo(combined(cv::Rect(debug.cols, 0, binary.cols, binary.rows)));
+
+        if (i > 0)
+            debug_icon_images.copyTo(combined(cv::Rect(0, debug.rows, debug_icon_images.cols, debug_icon_images.rows)));
+
+        imshow("Debug", combined);
 
         if (playing) video.update(1);
 
-        if (const auto key_press = cv::waitKey(1); key_press == 27) exit = true;
+        if (const auto key_press = cv::waitKey(0); key_press == 27) exit = true;
         else if (key_press == ' ') playing = !playing;
-        else if (key_press == ']') video.update(+01);
-        else if (key_press == '[') video.update(-01);
-        else if (key_press == '\\')video.update(+10);
-        else if (key_press == 'p') video.update(-10);
-        else if (key_press == '1') red.lower_bound[0]--;
-        else if (key_press == '2') red.lower_bound[0]++;
-        else if (key_press == '3') red.lower_bound[1]--;
-        else if (key_press == '4') red.lower_bound[1]++;
-        else if (key_press == '5') red.lower_bound[2]--;
-        else if (key_press == '6') red.lower_bound[2]++;
-
-        std::cout << "Lower Bound: " << red.lower_bound << std::endl;
+        else if (key_press == ']') video.update(+001);
+        else if (key_press == '[') video.update(-001);
+        else if (key_press == '\\')video.update(+100);
+        else if (key_press == 'p') video.update(-100);
     }
 
     cv::destroyAllWindows();
--- executable/svm/optimizer.cpp
+++ executable/svm/optimizer.cpp
@@ -2,127 +2,44 @@
 // Created by Sam Cheung on 7/19/24.
 //
 
-#include <opencv2/opencv.hpp>
-#include <opencv2/ml.hpp>
-
-#include <string>
-#include <algorithm>
-#include <filesystem>
-#include <random>
-
-auto list_directory_recursive(const std::filesystem::path& directory,
-                              const std::vector<std::string>& extension_whitelist = {})
-{
-    std::vector<std::filesystem::path> files;
-    for (const auto& p : std::filesystem::recursive_directory_iterator(directory))
-    {
-        if (std::filesystem::is_directory(p)) continue;
-        if (!extension_whitelist.empty())
-        {
-            auto extension = p.path().extension().string();
-            auto head = extension_whitelist.begin(),
-                 tail = extension_whitelist.end();
-            if (auto iterator = find(head, tail, extension); iterator != tail)
-                files.push_back(p.path());
-        }
-        else files.push_back(p.path());
-    }
-    return files;
-}
-
-auto read_image_recursive(const std::filesystem::path& directory)
-{
-    std::vector<cv::Mat> images;
-    const auto files = list_directory_recursive(directory, {".jpg"});
-    for_each(files.begin(), files.end(), [&images](const std::filesystem::path& p)
-    {
-        cv::Mat image = cv::imread(p.string());
-        resize(image, image, {20, 20}, 0, 0, cv::INTER_LINEAR);
-        image = image.reshape(1, 1);
-        image.convertTo(image, CV_32FC1);
-        images.push_back(image);
-    });
-    return images;
-}
-
-class dataset : public std::map<int, std::vector<cv::Mat>>
-{
-public:
-    std::vector<std::string> labels;
-
-    explicit dataset(const std::vector<std::string>& labels): labels(labels)
-    {
-    }
-
-    dataset(const std::filesystem::path& directory, const std::vector<std::string>& labels): labels(labels)
-    {
-        for (int i = 0; i < labels.size(); i++)
-        {
-            this->insert({i, read_image_recursive(directory / labels[i])});
-        }
-    }
-
-    std::tuple<dataset, dataset> sample(const float ratio = 0.8)
-    {
-        dataset head(labels), tail(labels);
-        for (auto& [index, images] : *this)
-        {
-            std::shuffle(images.begin(), images.end(),
-                         std::default_random_engine(std::chrono::system_clock::now().time_since_epoch().count()));
-            const auto split = static_cast<int>(static_cast<float>(images.size()) * ratio);
-            head.insert({index, {images.begin(), images.begin() + split}});
-            tail.insert({index, {images.begin() + split, images.end()}});
-        }
-        return {head, tail};
-    }
-};
+#include "rmcv.h"
 
 int main()
 {
-    dataset rm_labels("images/20240719/", {"1", "2", "3", "4", "5", "Sentry", "Negtive"});
+    rm::svm::dataset rm_labels("images/20240719/", {"1", "2", "3", "4", "5", "Sentry", "Negtive"});
 
     float correct = 0, total = 0;
-    for (int i = 0; i < 100; i++)
+
+    auto [training_set, validation_set] = rm_labels.sample(0.6);
+    auto [samples, responses] = format_data(training_set);
+
+    const cv::Ptr<cv::ml::SVM> svm = cv::ml::SVM::create();
+    svm->setType(cv::ml::SVM::C_SVC);
+    svm->setKernel(cv::ml::SVM::LINEAR);
+    svm->setTermCriteria(cv::TermCriteria(cv::TermCriteria::MAX_ITER + cv::TermCriteria::EPS, 1000, 1e-3));
+
+    int64 tick = cv::getTickCount();
+    svm->trainAuto(samples, cv::ml::ROW_SAMPLE, responses);
+
+    const double train_time = static_cast<double>(cv::getTickCount() - tick) / cv::getTickFrequency();
+    tick = cv::getTickCount();
+
+    for (const auto& [index, images] : validation_set)
     {
-        auto [training_set, validation_set] = rm_labels.sample(0.6);
-
-        cv::Mat samples, responses;
-        for (const auto& [index, images] : training_set)
+        for (auto& image : images)
         {
-            for (const auto& image : images)
-            {
-                samples.push_back(image);
-                responses.push_back(index);
-            }
+            if (index == static_cast<int>(svm->predict(image))) correct++;
+            total++;
         }
-
-        const cv::Ptr<cv::ml::SVM> svm = cv::ml::SVM::create();
-        svm->setType(cv::ml::SVM::C_SVC);
-        svm->setKernel(cv::ml::SVM::LINEAR);
-        svm->setTermCriteria(cv::TermCriteria(cv::TermCriteria::MAX_ITER + cv::TermCriteria::EPS, 1000, 1e-3));
-
-        int64 tick = cv::getTickCount();
-        svm->trainAuto(samples, cv::ml::ROW_SAMPLE, responses);
-
-        const double train_time = static_cast<double>(cv::getTickCount() - tick) / cv::getTickFrequency();
-        tick = cv::getTickCount();
-
-        for (const auto& [index, images] : validation_set)
-        {
-            for (auto& image : images)
-            {
-                if (index == static_cast<int>(svm->predict(image))) correct++;
-                total++;
-            }
-        }
-
-        const double inference_time = static_cast<double>(cv::getTickCount() - tick) / cv::getTickFrequency() * 1000 * 1000;
-        std::cout << i << " Accuracy: " << correct / total * 100 << "%, train time: " << train_time <<
-            "s, inference time: " << inference_time / total << "us." << std::endl;
     }
+
+    const double inference_time = static_cast<double>(cv::getTickCount() - tick) / cv::getTickFrequency() * 1000 * 1000;
+    std::cout << " Accuracy: " << correct / total * 100 << "%, train time: " << train_time <<
+        "s, inference time: " << inference_time / total << "us." << std::endl;
+
     std::cout << "Total Accuracy: " << correct / total * 100 << "%" << std::endl;
 
-    // svm->save("../svm.xml");
+    svm->save("svm.xml");
 
     return 0;
 }
